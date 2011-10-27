a4d0801b6deebfe99d5fcdc7aa501e9905d22d47
André Guerreiro <aguerreiro1985@gmail.com>
2011-10-27 19:02:42 +0000

    Updated inf file and makecmd script

--- pteid-mw/_src/eidmw/minidriver/makemsi/makemsi.cmd
+++ pteid-mw/_src/eidmw/minidriver/makemsi/makemsi.cmd
@@ -1,57 +1,69 @@
-@echo off
-set VERSION=4.0.10
+:: @echo off
+set VERSION=1.0.0
 
+cd makemsi
+
+set WIX_PATH="C:\Program Files (x86)\Windows Installer XML v3\bin"
 :: Inf2cat and signtool are installed by Windows Driver Kit (just check "Build Environments"):
 set INF2CAT_PATH=C:\WinDDK\7600.16385.1\bin\selfsign
 set SIGNTOOL_PATH=C:\WinDDK\7600.16385.1\bin\x86
 
 :: Certificate name and store
-::set CERTIFICATENAME=Fedict eID(test)
-::set CERTIFICATESTORE=PrivateCertStore
+set CERTIFICATENAME=Caixa Magica Software
+set CERTIFICATESTORE=PrivateCertStore
 :: To create a test certificate: 
 ::   %SIGNTOOL_PATH%\MakeCert.exe -r -pe -ss  %CERTIFICATESTORE% -n "CN=%CERTIFICATENAME%" fedicteidtest.cer
 
+:: Paths to compiled drivers
+:: We expect libbeidmdrv32_debug.dll, libbeidmdrv32.dll, libbeidmdrv64_debug.dll and libbeidmdrv64.dll 
+set BINPATH=../Win32\Debug
 
+:: Path to images
+set IMG_PATH=../img
 
-:: create minidriver driver installer
-:: ==================================
+set BUILDPATH=%~dp0
 
-:: BuildPath
-set INSTALLPATH=%~dp0
+:: cd %BUILDPATH%
 
-
-md %INSTALLPATH%\Release
-@echo [INFO] Copying minidriver files..
+md %BUILDPATH%\Debug
+md %BUILDPATH%\Release
+md %BUILDPATH%\Build
 
 :: copy inf files
-copy %INSTALLPATH%\beidmdrv.inf %INSTALLPATH%\Release
+copy beidmdrv.inf %BUILDPATH%\Debug
+copy beidmdrv.inf %BUILDPATH%\Release
 
 :: copy drivers. We use the same files for 32 and 64 bit. But we create architecture dependent MSI's
-copy %~dp0..\VS_2010\Binaries\Win32_Release\beidmdrv.dll %INSTALLPATH%\Release\beidmdrv32.dll
-copy %~dp0..\VS_2010\Binaries\x64_Release\beidmdrv.dll %INSTALLPATH%\Release\beidmdrv64.dll
+::copy %BINPATH%\beidmdrv32_debug.dll %BUILDPATH%\Debug\beidmdrv32.dll
+copy %BINPATH%\beidmdrv.dll %BUILDPATH%\Release\beidmdrv.dll
+::copy %BINPATH%\beidmdrv64_debug.dll %BUILDPATH%\Debug\beidmdrv64.dll
+::copy %BINPATH%\beidmdrv64.dll %BUILDPATH%\Release\beidmdrv64.dll
 
 :: copy icon
-copy %~dp0..\minidriver\img\beid.ico %INSTALLPATH%\Release\
+:: copy %IMG_PATH%\beid.ico %BUILDPATH%\Debug\
+copy %IMG_PATH%\beid.ico %BUILDPATH%\Release\
 
-@echo [INFO] Creating cat file
 :: Create catalog
-%INF2CAT_PATH%\inf2cat.exe /driver:%INSTALLPATH%\Release\ /os:XP_X86,XP_X64,Vista_X86,Vista_X64,7_X86,7_X64
-
-
-:: sign minidriver driver cat file
-:: ===============================
-
-:: Certificate name and store
-set CERTIFICATENAME=Fedict eID(test)
-set CERTIFICATESTORE=PrivateCertStore
-:: To create a test certificate: 
-@if exist "%~dp0fedicteidtest.cer" goto cert_exist
-
-@echo [INFO] Make cert
-%SIGNTOOL_PATH%\MakeCert.exe -r -pe -ss  %CERTIFICATESTORE% -n "CN=%CERTIFICATENAME%" %~dp0fedicteidtest.cer
-
-:cert_exist
+:: %INF2CAT_PATH%\inf2cat.exe /driver:%BUILDPATH%\Debug\ /os:XP_X86,XP_X64,Vista_X86,Vista_X64,7_X86,7_X64
+%INF2CAT_PATH%\inf2cat.exe /driver:%BUILDPATH%\Release\ /os:XP_X86,XP_X64,Vista_X86,Vista_X64,7_X86,7_X64
 
 :: Sign the catalog
-@echo [INFO] Sign the catalog
-%SIGNTOOL_PATH%\SignTool.exe sign /a /v /s %CERTIFICATESTORE% /n "%CERTIFICATENAME%"  /t http://timestamp.verisign.com/scripts/timestamp.dll %INSTALLPATH%\Release\beidmdrv.cat
+:: %SIGNTOOL_PATH%\SignTool.exe sign /v /s %CERTIFICATESTORE% /n "%CERTIFICATENAME%"  /t http://timestamp.verisign.com/scripts/timestamp.dll %BUILDPATH%\Debug\beidmdrv.cat
+%SIGNTOOL_PATH%\SignTool.exe sign /v /s %CERTIFICATESTORE% /n "%CERTIFICATENAME%"  /t http://timestamp.verisign.com/scripts/timestamp.dll %BUILDPATH%\Release\beidmdrv.cat
+
+:: Create MSI 64 bit Debug
+REM %WIX_PATH%\candle -dVersion=%VERSION% -ext %WIX_PATH%\WixDifxAppExtension.dll beidmdrv64debug.wxs 
+REM %WIX_PATH%\light -ext %WIX_PATH%\WixDifxAppExtension.dll -ext WixUIExtension beidmdrv64debug.wixobj %WIX_PATH%\difxapp_x64.wixlib -o Build\BeidMinidriver-%VERSION%-x64-Debug.msi
+
+:: Create MSI 64 bit Release
+:: %WIX_PATH%\candle -dVersion=%VERSION% -ext %WIX_PATH%\WixDifxAppExtension.dll beidmdrv64release.wxs 
+:: %WIX_PATH%\light -ext %WIX_PATH%\WixDifxAppExtension.dll -ext WixUIExtension beidmdrv64release.wixobj %WIX_PATH%\difxapp_x64.wixlib -o Build\BeidMinidriver-%VERSION%-x64.msi
+
+:: Create MSI 32 bit Debug
+:: %WIX_PATH%\candle -dVersion=%VERSION% -ext %WIX_PATH%\WixDifxAppExtension.dll beidmdrv32debug.wxs 
+:: %WIX_PATH%\light -ext %WIX_PATH%\WixDifxAppExtension.dll -ext WixUIExtension beidmdrv32debug.wixobj %WIX_PATH%\difxapp_x86.wixlib -o Build\BeidMinidriver-%VERSION%-x86-Debug.msi
+
+:: Create MSI 32 bit Release
+:: %WIX_PATH%\candle -dVersion=%VERSION% -ext %WIX_PATH%\WixDifxAppExtension.dll beidmdrv32release.wxs 
+:: %WIX_PATH%\light -ext %WIX_PATH%\WixDifxAppExtension.dll -ext WixUIExtension beidmdrv32release.wixobj %WIX_PATH%\difxapp_x86.wixlib -o Build\BeidMinidriver-%VERSION%-x86.msi
+
