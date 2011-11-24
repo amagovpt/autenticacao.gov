5666eb1614be0a73c6af810bb3c34536b935c4ab
lmcm <lmcm@caixamagica.pt>
2011-11-24 11:52:28 +0000

    Fix implementation.

--- pteid-mw-pt/_src/eidmw/applayer/sslclient.cpp
+++ pteid-mw-pt/_src/eidmw/applayer/sslclient.cpp
@@ -1,5 +1,5 @@
 /*
- * sslconnection.cpp
+ * sslclient.cpp
  *
  *  Created on: 2011/10/25
  *      Author: Luis Medinas <luis.medinas@caixamagica.pt>
@@ -27,260 +27,49 @@
 #include "sslcommon.h"
 #include "sslclient.h"
 
-#include <stdio.h>
-#include <openssl/rand.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-#include <openssl/engine.h>
-
-static const char *FIRST_POST=
-    "POST /CAPPINChange/connect HTTP/1.1\nHost: otp.cartaodecidadao.pt\nContent-Type: text/plain; charset=UTF-8\nContent-Length: 16\r\n\r\n{\"connect\":\"\"}\r\n";
-
-static const char *SECOND_POST=
-    "POST /CAPPINChange/sendParameters HTTP/1.1\r\nHost: otp.cartaodecidadao.pt\r\nContent-Type: text/plain; charset=UTF-8\r\n\r\n\r\n";
-
-static char *host="otp.cartaodecidadao.pt";
-static int port=443;
-static int require_server_auth=1;
-static char *ciphers=0;
-static int reconnect=1;
-
-/* Read from the POST and write to the server
-   Read from the server and write to the POST
-
-   we use select() to multiplex
-*/ 
-
-void read_write_buf(SSL * ssl, int sock, const char * buf, int len) 
+int tcp_connect(char *host,int port)
 {
-    int width;
-    int r,c2sl=0,c2s_offset=0;
-    int read_blocked_on_write=0,write_blocked_on_read=0,read_blocked=0;
-    fd_set readfds,writefds;
-    int shutdown_wait=0;
-    char c2s[BUFSIZZ],s2c[BUFSIZZ];
-    int ofcmode;
-    int rx_count = 0;
-    
-    /*First we make the socket nonblocking*/
-    ofcmode=fcntl(sock,F_GETFL,0);
-    ofcmode|=O_NDELAY;
-    if(fcntl(sock,F_SETFL,ofcmode))
-      err_exit("Couldn't make socket nonblocking");
-    
+	struct hostent *hp;
+	struct sockaddr_in addr;
+	int sock;
 
-    width=sock+1;
+	if(!(hp=gethostbyname(host)))
+		berr_exit((char *)"Couldn't resolve host");
+	memset(&addr,0,sizeof(addr));
+	addr.sin_addr=*(struct in_addr*)
+    		  hp->h_addr_list[0];
+	addr.sin_family=AF_INET;
+	addr.sin_port=htons(port);
 
-      /* Check for input on the console*/
-	strcpy(c2s, buf);
-	c2sl = strlen(c2s);
-       	c2s_offset=0;
-	printf("Init str bytes (%d): \n---\n string %s\n---\n", c2sl, c2s);
-    
-    while(1){
-      FD_ZERO(&readfds);
-      FD_ZERO(&writefds);
+	if((sock=socket(AF_INET,SOCK_STREAM,
+			IPPROTO_TCP))<0)
+		err_exit((char *)"Couldn't create socket");
+	if(connect(sock,(struct sockaddr *)&addr,
+			sizeof(addr))<0)
+		err_exit((char *)"Couldn't connect socket");
 
-      FD_SET(sock,&readfds);
-
-      /* If we're waiting for a read on the socket don't
-         try to write to the server */
-      if(!write_blocked_on_read){
-        /* If we have data in the write queue don't try to
-           read from stdin */
-        if(c2sl || read_blocked_on_write) {
-		printf("Using Write FDS only\n");
-          FD_SET(sock,&writefds);
-	}
-      }
-      
-      r=select(width,&readfds,&writefds,0,0);
-      if(r==0)
-        continue;
-
-      /* Now check if there's data to read */
-      if((FD_ISSET(sock,&readfds) && !write_blocked_on_read) ||
-        (read_blocked_on_write && FD_ISSET(sock,&writefds))){
-        do {
-          read_blocked_on_write=0;
-          read_blocked=0;
-          
-          r=SSL_read(ssl,s2c,BUFSIZZ);
-          
-          switch(SSL_get_error(ssl,r)){
-            case SSL_ERROR_NONE:
-              /* Note: this call could block, which blocks the
-                 entire application. It's arguable this is the
-                 right behavior since this is essentially a terminal
-                 client. However, in some other applications you
-                 would have to prevent this condition */
-              fwrite(s2c,1,r,stdout);
-		fflush(stdout);
-
-		rx_count++;
-
-		/* rx header and body */
-		if ( rx_count == 2)
-			return;
-		
-		
-/* 
-		if ( r == 14) {
-			printf(">>>> Success: exiting read_write\n");
-			goto end;
-		}
- */
-
-              break;
-            case SSL_ERROR_ZERO_RETURN:
-              /* End of data */
-              if(!shutdown_wait)
-                SSL_shutdown(ssl);
-              goto end;
-              break;
-            case SSL_ERROR_WANT_READ:
-		printf("Read block WANT_READ on Write:  SSL_ERROR_WANT_READ\n");
-              read_blocked=1;
-              break;
-              
-              /* We get a WANT_WRITE if we're
-                 trying to rehandshake and we block on
-                 a write during that rehandshake.
-
-                 We need to wait on the socket to be 
-                 writeable but reinitiate the read
-                 when it is */
-            case SSL_ERROR_WANT_WRITE:
-              read_blocked_on_write=1;
-		printf("Read block on Write:  SSL_ERROR_WANT_WRITE\n");
-              break;
-            default:
-              berr_exit("SSL read problem");
-          }
-
-          /* We need a check for read_blocked here because
-             SSL_pending() doesn't work properly during the
-             handshake. This check prevents a busy-wait
-             loop around SSL_read() */
-        } while (SSL_pending(ssl) && !read_blocked);
-      }
-      
-      /* If the socket is writeable... */
-      if((FD_ISSET(sock,&writefds) && c2sl) ||
-        (write_blocked_on_read && FD_ISSET(sock,&readfds))) {
-        write_blocked_on_read=0;
-
-        /* Try to write */
-        r=SSL_write(ssl,c2s+c2s_offset,c2sl);
-
-	printf("SSL_write: Sent %d bytes\n", r);
-          
-        switch(SSL_get_error(ssl,r)){
-          /* We wrote something*/
-          case SSL_ERROR_NONE:
-            c2sl-=r;
-            c2s_offset+=r;
-
-		if (c2sl == 0) {
-			printf("Finished sending buf\n");
-		}
-            break;
-              
-            /* We would have blocked */
-          case SSL_ERROR_WANT_WRITE:
-            break;
-
-            /* We get a WANT_READ if we're
-               trying to rehandshake and we block on
-               write during the current connection.
-               
-               We need to wait on the socket to be readable
-               but reinitiate our write when it is */
-          case SSL_ERROR_WANT_READ:
-            write_blocked_on_read=1;
-		printf("(SSL_WRITE) Write blocked on WANT_READ on Write\n");
-
-            break;
-              
-              /* Some other error */
-          default:	      
-            berr_exit("SSL write problem");
-        }
-      }
-    }
-      
-  end:
-    return;
+	return sock;
 }
 
-
-
-int main (int argc,char *argv[])
+/* Check that the common name matches the
+   host name*/
+void check_cert(SSL *ssl, char *host)
 {
-	SSL_CTX *ctx;
-	SSL *ssl;
-	BIO *sbio;
-	int sock;
-	extern char *optarg;
+	X509 *peer;
+	char peer_CN[256];
 
-	SSL_SESSION *sess;
-	/* Build our SSL context*/
-	ctx=initialize_ctx((char *)KEYFILE);
+	if(SSL_get_verify_result(ssl)!=X509_V_OK)
+		berr_exit((char *)"Certificate doesn't verify");
 
-	if(ciphers){
-		SSL_CTX_set_cipher_list(ctx,ciphers);
-	}
+	/*Check the cert chain. The chain length
+      is automatically checked by OpenSSL when
+      we set the verify depth in the ctx */
 
-
-	/* Connect the TCP socket*/
-	sock=tcp_connect(host,port);
-
-	/* Connect the SSL socket */
-	ssl=SSL_new(ctx);
-	sbio=BIO_new_socket(sock,BIO_NOCLOSE);
-	SSL_set_bio(ssl,sbio,sbio);
-
-	if(SSL_connect(ssl)<=0)
-		berr_exit((char *)"SSL connect error");
-	if(require_server_auth)
-		check_cert(ssl,host);
-
-	/* Now hang up and reconnect, if requested */
-	if(reconnect) {
-		sess=SSL_get1_session(ssl); /*Collect the session*/
-		SSL_shutdown(ssl);
-		SSL_free(ssl);
-		close(sock);
-
-		sock=tcp_connect(host,port);
-		ssl=SSL_new(ctx);
-		sbio=BIO_new_socket(sock,BIO_NOCLOSE);
-		SSL_set_bio(ssl,sbio,sbio);
-		SSL_set_session(ssl,sess); /*And resume it*/
-		if(SSL_connect(ssl)<=0)
-			berr_exit((char *)"SSL connect error (second connect)");
-		check_cert(ssl,host);
-	}
-
-	/* Now make our HTTP request */
-
-	printf("\nREQUEST - 1\n");
-	char buf[30000];
-	memset(buf, 0, 30000);
-	int len = 0;
-	strcpy (buf, FIRST_POST);
-	len = strlen(buf);
-	read_write_buf(ssl, sock, buf, len);
-
-	printf("\nREQUEST - 2\n");
-	read_write_buf(ssl, sock, SECOND_POST, len);
-
-    	SSL_free(ssl);
-	close(sock);
-	//read_write(ssl,sock);
-	/* Shutdown the socket */
-	destroy_ctx(ctx);
-	close(sock);
-
-	exit(0);
+	/*Check the common name*/
+	peer=SSL_get_peer_certificate(ssl);
+	X509_NAME_get_text_by_NID
+	(X509_get_subject_name(peer),
+			NID_commonName, peer_CN, 256);
+	if(strcasecmp(peer_CN,host))
+		err_exit ((char *)"Common name doesn't match host name");
 }
--- pteid-mw-pt/_src/eidmw/applayer/sslconnection.cpp
+++ pteid-mw-pt/_src/eidmw/applayer/sslconnection.cpp
@@ -26,104 +26,194 @@
 
 #include "sslcommon.h"
 #include "sslclient.h"
-#include "sslconnection.h"
 
+#include <stdio.h>
 #include <openssl/rand.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <openssl/engine.h>
 
-/*static char *REQUEST_TEMPLATE=
-   "GET / HTTP/1.0\r\nUser-Agent:"
-   "EKRClient\r\nHost: %s:%d\r\n\r\n";*/
+static const char *FIRST_POST=
+    "POST /CAPPINChange/connect HTTP/1.1\nHost: otp.cartaodecidadao.pt\nContent-Type: text/plain; charset=UTF-8\nContent-Length: 16\r\n\r\n{\"connect\":\"\"}\r\n";
 
-static const char *REQUEST_TEMPLATE=
-		"POST /CAPPINChange/connect HTTP/1.1\r\nHost: otp.cartaodecidadao.pt\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Length: 16\r\n\r\n{\"connect\":\"\"}\r\n";
+static const char *SECOND_POST=
+    "POST /CAPPINChange/sendParameters HTTP/1.1\r\nHost: otp.cartaodecidadao.pt\r\nContent-Type: text/plain; charset=UTF-8\r\n\r\n\r\n";
 
 static char *host="otp.cartaodecidadao.pt";
-//static char *host="localhost";
 static int port=443;
 static int require_server_auth=1;
 static char *ciphers=0;
 static int reconnect=1;
 
-static int http_request(SSL *ssl)
+/* Read from the POST and write to the server
+   Read from the server and write to the POST
+
+   we use select() to multiplex
+*/ 
+
+void read_write_buf(SSL * ssl, int sock, const char * buf, int len) 
 {
-	char *request=0;
-	char buf[BUFSIZZ];
-	int r;
-	int len, request_len;
+    int width;
+    int r,c2sl=0,c2s_offset=0;
+    int read_blocked_on_write=0,write_blocked_on_read=0,read_blocked=0;
+    fd_set readfds,writefds;
+    int shutdown_wait=0;
+    char c2s[BUFSIZZ],s2c[BUFSIZZ];
+    int ofcmode;
+    int rx_count = 0;
+    
+    /*First we make the socket nonblocking*/
+    ofcmode=fcntl(sock,F_GETFL,0);
+    ofcmode|=O_NDELAY;
+    if(fcntl(sock,F_SETFL,ofcmode))
+      err_exit("Couldn't make socket nonblocking");
+    
 
-	/* Now construct our HTTP request */
-	request_len=strlen(REQUEST_TEMPLATE)+
-			strlen(host)+6;
-	if(!(request=(char *)malloc(request_len)))
-		err_exit((char *)"Couldn't allocate request");
-	snprintf(request,request_len,REQUEST_TEMPLATE,
-			host,port);
+    width=sock+1;
 
-	/* Find the exact request_len */
-	request_len=strlen(request);
+      /* Check for input on the console*/
+	strcpy(c2s, buf);
+	c2sl = strlen(c2s);
+       	c2s_offset=0;
+	printf("Init str bytes (%d): \n---\n string %s\n---\n", c2sl, c2s);
+    
+    while(1){
+      FD_ZERO(&readfds);
+      FD_ZERO(&writefds);
 
-	r=SSL_write(ssl,request,request_len);
-	switch(SSL_get_error(ssl,r)){
-	case SSL_ERROR_NONE:
-		if(request_len!=r)
-			err_exit((char *)"Incomplete write!");
-		break;
-	default:
-		berr_exit((char *)"SSL write problem");
+      FD_SET(sock,&readfds);
+
+      /* If we're waiting for a read on the socket don't
+         try to write to the server */
+      if(!write_blocked_on_read){
+        /* If we have data in the write queue don't try to
+           read from stdin */
+        if(c2sl || read_blocked_on_write) {
+		printf("Using Write FDS only\n");
+          FD_SET(sock,&writefds);
 	}
+      }
+      
+      r=select(width,&readfds,&writefds,0,0);
+      if(r==0)
+        continue;
 
-	/* Now read the server's response, assuming
-       that it's terminated by a close */
-	while(1){
-		r=SSL_read(ssl,buf,BUFSIZZ);
-		switch(SSL_get_error(ssl,r)){
-		case SSL_ERROR_NONE:
-			len=r;
-			break;
-		case SSL_ERROR_WANT_READ:
-			continue;
-		case SSL_ERROR_ZERO_RETURN:
-			goto shutdown;
-		case SSL_ERROR_SYSCALL:
-			fprintf(stderr,
-					"SSL Error: Premature close\n");
-			goto done;
-		default:
-			berr_exit((char *)"SSL read problem wclient.c");
+      /* Now check if there's data to read */
+      if((FD_ISSET(sock,&readfds) && !write_blocked_on_read) ||
+        (read_blocked_on_write && FD_ISSET(sock,&writefds))){
+        do {
+          read_blocked_on_write=0;
+          read_blocked=0;
+          
+          r=SSL_read(ssl,s2c,BUFSIZZ);
+          
+          switch(SSL_get_error(ssl,r)){
+            case SSL_ERROR_NONE:
+              /* Note: this call could block, which blocks the
+                 entire application. It's arguable this is the
+                 right behavior since this is essentially a terminal
+                 client. However, in some other applications you
+                 would have to prevent this condition */
+              fwrite(s2c,1,r,stdout);
+		fflush(stdout);
+
+		rx_count++;
+
+		/* rx header and body */
+		if ( rx_count == 2)
+			return;
+		
+		
+/* 
+		if ( r == 14) {
+			printf(">>>> Success: exiting read_write\n");
+			goto end;
 		}
+ */
 
-		buf[r] = '\0';
-		FILE *fp;
-		fwrite(buf,1,len,stdout);
-		fread (buf, 1, len, stdout);
+              break;
+            case SSL_ERROR_ZERO_RETURN:
+              /* End of data */
+              if(!shutdown_wait)
+                SSL_shutdown(ssl);
+              goto end;
+              break;
+            case SSL_ERROR_WANT_READ:
+		printf("Read block WANT_READ on Write:  SSL_ERROR_WANT_READ\n");
+              read_blocked=1;
+              break;
+              
+              /* We get a WANT_WRITE if we're
+                 trying to rehandshake and we block on
+                 a write during that rehandshake.
 
-		char token[190];
+                 We need to wait on the socket to be 
+                 writeable but reinitiate the read
+                 when it is */
+            case SSL_ERROR_WANT_WRITE:
+              read_blocked_on_write=1;
+		printf("Read block on Write:  SSL_ERROR_WANT_WRITE\n");
+              break;
+            default:
+              berr_exit("SSL read problem");
+          }
 
-		strncpy (token, buf+180, 67);
-		token[67] = '\0';
-		printf ("%s", token);
+          /* We need a check for read_blocked here because
+             SSL_pending() doesn't work properly during the
+             handshake. This check prevents a busy-wait
+             loop around SSL_read() */
+        } while (SSL_pending(ssl) && !read_blocked);
+      }
+      
+      /* If the socket is writeable... */
+      if((FD_ISSET(sock,&writefds) && c2sl) ||
+        (write_blocked_on_read && FD_ISSET(sock,&readfds))) {
+        write_blocked_on_read=0;
 
-	}
+        /* Try to write */
+        r=SSL_write(ssl,c2s+c2s_offset,c2sl);
 
-	shutdown:
-	r=SSL_shutdown(ssl);
-	switch(r){
-	case 1:
-		break; /* Success */
-	case 0:
-	case -1:
-	default:
-		berr_exit((char *)"Shutdown failed");
-	}
+	printf("SSL_write: Sent %d bytes\n", r);
+          
+        switch(SSL_get_error(ssl,r)){
+          /* We wrote something*/
+          case SSL_ERROR_NONE:
+            c2sl-=r;
+            c2s_offset+=r;
 
-	done:
-	SSL_free(ssl);
-	free(request);
-	return(0);
+		if (c2sl == 0) {
+			printf("Finished sending buf\n");
+		}
+            break;
+              
+            /* We would have blocked */
+          case SSL_ERROR_WANT_WRITE:
+            break;
+
+            /* We get a WANT_READ if we're
+               trying to rehandshake and we block on
+               write during the current connection.
+               
+               We need to wait on the socket to be readable
+               but reinitiate our write when it is */
+          case SSL_ERROR_WANT_READ:
+            write_blocked_on_read=1;
+		printf("(SSL_WRITE) Write blocked on WANT_READ on Write\n");
+
+            break;
+              
+              /* Some other error */
+          default:	      
+            berr_exit("SSL write problem");
+        }
+      }
+    }
+      
+  end:
+    return;
 }
+
+
 
 int init ()
 {
@@ -132,7 +222,6 @@ int init ()
 	BIO *sbio;
 	int sock;
 	extern char *optarg;
-	int c;
 
 	SSL_SESSION *sess;
 	/* Build our SSL context*/
@@ -174,9 +263,20 @@ int init ()
 	}
 
 	/* Now make our HTTP request */
-	http_request(ssl);
+
+	printf("\nREQUEST - 1\n");
+	char buf[30000];
+	memset(buf, 0, 30000);
+	int len = 0;
+	strcpy (buf, FIRST_POST);
+	len = strlen(buf);
+	read_write_buf(ssl, sock, buf, len);
+
+	printf("\nREQUEST - 2\n");
+	read_write_buf(ssl, sock, SECOND_POST, len);
 
 	/* Shutdown the socket */
+	SSL_free(ssl);
 	destroy_ctx(ctx);
 	close(sock);
 
