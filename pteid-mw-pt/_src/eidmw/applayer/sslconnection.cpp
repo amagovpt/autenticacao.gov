6a857d26327d7659d67ab27a5e1bbbc24caf4f5b
lmcm <lmcm@caixamagica.pt>
2011-11-24 12:37:03 +0000

    Better ident.

--- pteid-mw-pt/_src/eidmw/applayer/sslconnection.cpp
+++ pteid-mw-pt/_src/eidmw/applayer/sslconnection.cpp
@@ -53,164 +53,164 @@ static int reconnect=1;
 
 void read_write_buf(SSL * ssl, int sock, const char * buf, int len) 
 {
-    int width;
-    int r,c2sl=0,c2s_offset=0;
-    int read_blocked_on_write=0,write_blocked_on_read=0,read_blocked=0;
-    fd_set readfds,writefds;
-    int shutdown_wait=0;
-    char c2s[BUFSIZZ],s2c[BUFSIZZ];
-    int ofcmode;
-    int rx_count = 0;
-    
-    /*First we make the socket nonblocking*/
-    ofcmode=fcntl(sock,F_GETFL,0);
-    ofcmode|=O_NDELAY;
-    if(fcntl(sock,F_SETFL,ofcmode))
-      err_exit("Couldn't make socket nonblocking");
-    
+	int width;
+	int r,c2sl=0,c2s_offset=0;
+	int read_blocked_on_write=0,write_blocked_on_read=0,read_blocked=0;
+	fd_set readfds,writefds;
+	int shutdown_wait=0;
+	char c2s[BUFSIZZ],s2c[BUFSIZZ];
+	int ofcmode;
+	int rx_count = 0;
 
-    width=sock+1;
+	/*First we make the socket nonblocking*/
+	ofcmode=fcntl(sock,F_GETFL,0);
+	ofcmode|=O_NDELAY;
+	if(fcntl(sock,F_SETFL,ofcmode))
+		err_exit("Couldn't make socket nonblocking");
 
-      /* Check for input on the console*/
+
+	width=sock+1;
+
+	/* Check for input on the console*/
 	strcpy(c2s, buf);
 	c2sl = strlen(c2s);
-       	c2s_offset=0;
+	c2s_offset=0;
 	printf("Init str bytes (%d): \n---\n string %s\n---\n", c2sl, c2s);
-    
-    while(1){
-      FD_ZERO(&readfds);
-      FD_ZERO(&writefds);
 
-      FD_SET(sock,&readfds);
+	while(1){
+		FD_ZERO(&readfds);
+		FD_ZERO(&writefds);
 
-      /* If we're waiting for a read on the socket don't
+		FD_SET(sock,&readfds);
+
+		/* If we're waiting for a read on the socket don't
          try to write to the server */
-      if(!write_blocked_on_read){
-        /* If we have data in the write queue don't try to
+		if(!write_blocked_on_read){
+			/* If we have data in the write queue don't try to
            read from stdin */
-        if(c2sl || read_blocked_on_write) {
-		printf("Using Write FDS only\n");
-          FD_SET(sock,&writefds);
-	}
-      }
-      
-      r=select(width,&readfds,&writefds,0,0);
-      if(r==0)
-        continue;
-
-      /* Now check if there's data to read */
-      if((FD_ISSET(sock,&readfds) && !write_blocked_on_read) ||
-        (read_blocked_on_write && FD_ISSET(sock,&writefds))){
-        do {
-          read_blocked_on_write=0;
-          read_blocked=0;
-          
-          r=SSL_read(ssl,s2c,BUFSIZZ);
-          
-          switch(SSL_get_error(ssl,r)){
-            case SSL_ERROR_NONE:
-              /* Note: this call could block, which blocks the
-                 entire application. It's arguable this is the
-                 right behavior since this is essentially a terminal
-                 client. However, in some other applications you
-                 would have to prevent this condition */
-              fwrite(s2c,1,r,stdout);
-		fflush(stdout);
-
-		rx_count++;
-
-		/* rx header and body */
-		if ( rx_count == 2)
-			return;
-		
-		
-/* 
-		if ( r == 14) {
-			printf(">>>> Success: exiting read_write\n");
-			goto end;
+			if(c2sl || read_blocked_on_write) {
+				printf("Using Write FDS only\n");
+				FD_SET(sock,&writefds);
+			}
 		}
- */
 
-              break;
-            case SSL_ERROR_ZERO_RETURN:
-              /* End of data */
-              if(!shutdown_wait)
-                SSL_shutdown(ssl);
-              goto end;
-              break;
-            case SSL_ERROR_WANT_READ:
-		printf("Read block WANT_READ on Write:  SSL_ERROR_WANT_READ\n");
-              read_blocked=1;
-              break;
-              
-              /* We get a WANT_WRITE if we're
-                 trying to rehandshake and we block on
-                 a write during that rehandshake.
+		r=select(width,&readfds,&writefds,0,0);
+		if(r==0)
+			continue;
 
-                 We need to wait on the socket to be 
-                 writeable but reinitiate the read
-                 when it is */
-            case SSL_ERROR_WANT_WRITE:
-              read_blocked_on_write=1;
-		printf("Read block on Write:  SSL_ERROR_WANT_WRITE\n");
-              break;
-            default:
-              berr_exit("SSL read problem");
-          }
+		/* Now check if there's data to read */
+		if((FD_ISSET(sock,&readfds) && !write_blocked_on_read) ||
+				(read_blocked_on_write && FD_ISSET(sock,&writefds))){
+			do {
+				read_blocked_on_write=0;
+				read_blocked=0;
 
-          /* We need a check for read_blocked here because
-             SSL_pending() doesn't work properly during the
-             handshake. This check prevents a busy-wait
-             loop around SSL_read() */
-        } while (SSL_pending(ssl) && !read_blocked);
-      }
-      
-      /* If the socket is writeable... */
-      if((FD_ISSET(sock,&writefds) && c2sl) ||
-        (write_blocked_on_read && FD_ISSET(sock,&readfds))) {
-        write_blocked_on_read=0;
+				r=SSL_read(ssl,s2c,BUFSIZZ);
 
-        /* Try to write */
-        r=SSL_write(ssl,c2s+c2s_offset,c2sl);
+				switch(SSL_get_error(ssl,r)){
+				case SSL_ERROR_NONE:
+					/* Note: this call could block, which blocks the
+					 entire application. It's arguable this is the
+					 right behavior since this is essentially a terminal
+					 client. However, in some other applications you
+					 would have to prevent this condition */
+					fwrite(s2c,1,r,stdout);
+					fflush(stdout);
 
-	printf("SSL_write: Sent %d bytes\n", r);
-          
-        switch(SSL_get_error(ssl,r)){
-          /* We wrote something*/
-          case SSL_ERROR_NONE:
-            c2sl-=r;
-            c2s_offset+=r;
+					rx_count++;
 
-		if (c2sl == 0) {
-			printf("Finished sending buf\n");
+					/* rx header and body */
+					if ( rx_count == 2)
+						return;
+
+
+					/*
+					if ( r == 14) {
+						printf(">>>> Success: exiting read_write\n");
+						goto end;
+					}
+					*/
+
+					break;
+				case SSL_ERROR_ZERO_RETURN:
+					/* End of data */
+					if(!shutdown_wait)
+						SSL_shutdown(ssl);
+					goto end;
+					break;
+				case SSL_ERROR_WANT_READ:
+					printf("Read block WANT_READ on Write:  SSL_ERROR_WANT_READ\n");
+					read_blocked=1;
+					break;
+
+					/* We get a WANT_WRITE if we're
+					 trying to rehandshake and we block on
+					 a write during that rehandshake.
+
+					 We need to wait on the socket to be
+					 writeable but reinitiate the read
+					 when it is */
+				case SSL_ERROR_WANT_WRITE:
+					read_blocked_on_write=1;
+					printf("Read block on Write:  SSL_ERROR_WANT_WRITE\n");
+					break;
+				default:
+					berr_exit("SSL read problem");
+				}
+
+				/* We need a check for read_blocked here because
+				 SSL_pending() doesn't work properly during the
+				 handshake. This check prevents a busy-wait
+				 loop around SSL_read() */
+			} while (SSL_pending(ssl) && !read_blocked);
 		}
-            break;
-              
-            /* We would have blocked */
-          case SSL_ERROR_WANT_WRITE:
-            break;
 
-            /* We get a WANT_READ if we're
+		/* If the socket is writeable... */
+		if((FD_ISSET(sock,&writefds) && c2sl) ||
+				(write_blocked_on_read && FD_ISSET(sock,&readfds))) {
+			write_blocked_on_read=0;
+
+			/* Try to write */
+			r=SSL_write(ssl,c2s+c2s_offset,c2sl);
+
+			printf("SSL_write: Sent %d bytes\n", r);
+
+			switch(SSL_get_error(ssl,r)){
+			/* We wrote something*/
+			case SSL_ERROR_NONE:
+				c2sl-=r;
+				c2s_offset+=r;
+
+				if (c2sl == 0) {
+					printf("Finished sending buf\n");
+				}
+				break;
+
+				/* We would have blocked */
+			case SSL_ERROR_WANT_WRITE:
+				break;
+
+				/* We get a WANT_READ if we're
                trying to rehandshake and we block on
                write during the current connection.
-               
+
                We need to wait on the socket to be readable
                but reinitiate our write when it is */
-          case SSL_ERROR_WANT_READ:
-            write_blocked_on_read=1;
-		printf("(SSL_WRITE) Write blocked on WANT_READ on Write\n");
+			case SSL_ERROR_WANT_READ:
+				write_blocked_on_read=1;
+				printf("(SSL_WRITE) Write blocked on WANT_READ on Write\n");
 
-            break;
-              
-              /* Some other error */
-          default:	      
-            berr_exit("SSL write problem");
-        }
-      }
-    }
-      
-  end:
-    return;
+				break;
+
+				/* Some other error */
+			default:
+				berr_exit("SSL write problem");
+			}
+		}
+	}
+
+	end:
+	return;
 }
 
 
