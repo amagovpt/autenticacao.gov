ee22aa8b1f9052abd83c60ce1f93d4a516830225
André Guerreiro <aguerreiro1985@gmail.com>
2011-12-23 14:36:19 +0000

    Revert mw-online patch applied on commit 1081

--- pteid-mw-pt/_src/eidmw/cardlayer/cardpluginPteid/PteidCard.cpp
+++ pteid-mw-pt/_src/eidmw/cardlayer/cardpluginPteid/PteidCard.cpp
@@ -721,14 +721,14 @@ CByteArray CPteidCard::SignInternal(const tPrivKey & key, unsigned long algo,
 
     // Pretty unique for smart cards: first MSE SET, then verify PIN
     // (needed for the nonrep key/pin, but also usable for the auth key/pin)
-    /*if (pPin != NULL)
+    if (pPin != NULL)
     {
         unsigned long ulRemaining = 0;
         bool bOK = PinCmd(PIN_OP_VERIFY, *pPin, "", "", ulRemaining, &key);
         if (!bOK)
 			throw CMWEXCEPTION(ulRemaining == 0 ? EIDMW_ERR_PIN_BLOCKED : EIDMW_ERR_PIN_BAD);
     }
-	*/
+	
 
     SetSecurityEnv(key, algo, oData.Size());
 
