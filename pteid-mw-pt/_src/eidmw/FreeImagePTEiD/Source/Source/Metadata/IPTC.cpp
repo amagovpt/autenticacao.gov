b03d90743e3b6dbfa549dd47810c30d89954d93a
rmartinho <rmartinho@caixamagica.pt>
2012-01-04 12:08:38 +0000

    removed jpeg support added png support

--- pteid-mw-pt/_src/eidmw/FreeImagePTEiD/Source/Source/Metadata/IPTC.cpp
+++ pteid-mw-pt/_src/eidmw/FreeImagePTEiD/Source/Source/Metadata/IPTC.cpp
@@ -23,7 +23,7 @@
 #pragma warning (disable : 4786) // identifier was truncated to 'number' characters
 #endif
 
-#include "FreeImagePTEiD.h"
+#include "FreeImage.h"
 #include "Utilities.h"
 #include "FreeImageTag.h"
 
--- pteid-mw-pt/_src/eidmw/FreeImagePTEiD/gensrclist.sh
+++ pteid-mw-pt/_src/eidmw/FreeImagePTEiD/gensrclist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIRLIST=". Source Source/Metadata Source/FreeImageToolkit Source/LibJPEG Source/LibMNG Source/LibPNG Source/LibTIFF Source/ZLib Source/LibOpenJPEG Source/OpenEXR Source/OpenEXR/Half Source/OpenEXR/Iex Source/OpenEXR/IlmImf Source/OpenEXR/IlmThread Source/OpenEXR/Imath Source/LibRawLite Source/LibRawLite/dcraw Source/LibRawLite/internal Source/LibRawLite/libraw Source/LibRawLite/src"
+DIRLIST=". Source Source/Metadata Source/FreeImageToolkit Source/LibPNG Source/ZLib Source/LibOpenJPEG"
 
 echo "VER_MAJOR = 3" > Makefile.srcs
 echo "VER_MINOR = 15.1" >> Makefile.srcs
