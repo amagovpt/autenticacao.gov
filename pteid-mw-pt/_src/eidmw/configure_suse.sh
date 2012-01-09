12a20ee2144ca10e67ddd9bff07a8dc05744dbb1
lmcm <lmcm@caixamagica.pt>
2012-01-09 14:45:20 +0000

    Add support for x86_64 and 32bits build.

--- pteid-mw-pt/_src/eidmw/configure_suse.sh
+++ pteid-mw-pt/_src/eidmw/configure_suse.sh
@@ -9,7 +9,16 @@ then
 	JAVAINCPATH=${JAVACPATH%/*}
 	JAVAINCPATH=$JAVAINCPATH/../include
 fi
-QT4DIR="/usr/lib64/qt4"
+
+ARCH="uname -m"
+
+if [[ $ARCH -eq "x86_64" ]]
+then
+    QT4DIR="/usr/lib64/qt4"
+else
+    QT4DIR="/usr/lib/qt4"
+fi
+
 QT4=`which qmake 2> /dev/null | wc -l`
 if [ $QT4 -eq 1 ]
 then
@@ -19,7 +28,7 @@ fi
 
 if [ $QT4DIR != "" ]
 then
-./configure --prefix=${QT4DIR}/.. CONFIG=release CONFIG+=no_lflags_merge --lib+=-L${QT4DIR}/../lib64 --include+=${QT4DIR}/../include/Qt --include+=$JAVAINCPATH --include+=$JAVAINCPATH/linux  BUILD_SDK=1
+./configure --prefix=${QT4DIR}/.. CONFIG=release CONFIG+=no_lflags_merge --lib+=-L${QT4DIR}/../lib --include+=${QT4DIR}/../include/Qt --include+=$JAVAINCPATH --include+=$JAVAINCPATH/linux  BUILD_SDK=1
 else
 ./configure CONFIG=release CONFIG+=no_lflags_merge --include+=/usr/include/Qt --include+=/usr/include/QtGui --include+=$JAVAINCPATH --include+=$JAVAINCPATH/linux  BUILD_SDK=1
 fi
