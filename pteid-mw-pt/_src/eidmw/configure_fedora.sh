93bba6e38c62a6d67abe48bc189187e2f2799d4c
lmcm <lmcm@caixamagica.pt>
2012-01-10 12:52:49 +0000

    Add x86_64 variable.

--- pteid-mw-pt/_src/eidmw/configure_fedora.sh
+++ pteid-mw-pt/_src/eidmw/configure_fedora.sh
@@ -6,6 +6,8 @@
 ##########################
 #QT4DIR=`stat -c %n /usr/local/Trolltech/Qt*/bin 2> /dev/null` 
 QT4DIR=`stat -c %n /usr/lib/qt4/bin 2> /dev/null`
+ARCH="uname -m"
+
 if [ $QT4DIR != "" ]
 then
 ./configure --lib+=-L${QT4DIR}/../lib --lib+=-L/usr/lib/xerces-c --include+=/usr/include/xercesc --include+=${QT4DIR}/../include/Qt BUILD_SDK=1
