9350d0179c5eff5952b0b65a8bacb0bf0677844c
André Guerreiro <aguerreiro1985@gmail.com>
2012-02-12 22:23:07 +0000

    GemPC Pinpad: Fixed Windows ioctl's and added PT strings on the Reader Display.

--- pteid-mw-pt/_src/eidmw/cardlayer/ppgempc-plugin/pinpad-gempc-plugin.c
+++ pteid-mw-pt/_src/eidmw/cardlayer/ppgempc-plugin/pinpad-gempc-plugin.c
@@ -36,12 +36,11 @@ EIDMW_PP_API long EIDMW_PP2_Init(
 
 }
 
-void printHex(char *array, unsigned int len)
+void printHex(unsigned char *arr, unsigned int len)
 {
 	unsigned int i = 0;
-	char *p = array;
 
-	if (array == NULL )
+	if (arr == NULL )
 	{   
 		printf("printfHex: NULL Array\n");
 		return;
@@ -54,7 +53,7 @@ void printHex(char *array, unsigned int len)
 			printf("\n");
 		}   
 
-		printf ("%02X ", *p++);
+		printf ("%02X ", arr[i]);
 		i++;
 	}   
 	printf("\n\n");
@@ -145,10 +144,10 @@ DLL_LOCAL void fillVerifyControlStruct(EIDMW_PP_VERIFY_CCID * pin_verify)
 {
 
 	/* PC/SC v2.0.2 Part 10 PIN verification data structure */
-	pin_verify -> bTimerOut = 0; 
+	pin_verify -> bTimerOut = 0x1E; 
 	pin_verify -> bTimerOut2 = 0x1E;   //30 seconds timeout
 	pin_verify -> bmFormatString = 0x82;
-	pin_verify -> bmPINBlockString = 0x04;
+	pin_verify -> bmPINBlockString = 0x00; //On Linux it can be 0x04!!!
 	pin_verify -> bmPINLengthFormat = 0x00;
 	(pin_verify -> wPINMaxExtraDigit)[0] = 0x08; /* Min Max */
 	pin_verify -> wPINMaxExtraDigit[1] = 0x04; 
@@ -167,7 +166,7 @@ DLL_LOCAL void fillVerifyControlStruct(EIDMW_PP_VERIFY_CCID * pin_verify)
 DLL_LOCAL void fillModifyControlStruct(EIDMW_PP_CHANGE_CCID * pin_change)
 {
 
-	pin_change -> bTimerOut = 0; 
+	pin_change -> bTimerOut = 0x1E; 
 	pin_change -> bTimerOut2 = 0x1E;   //30 seconds timeout
 	pin_change -> bmFormatString = 0x82;
 	pin_change -> bmPINBlockString = 0x04;
@@ -249,9 +248,13 @@ DLL_LOCAL DWORD sendControl(SCARDHANDLE hCard, int ioctl,
 {
 	DWORD rv = 0;
 	int i = 0;
+	
+	printf("SCardControl() Parameters: \n");
+	printf("ioctl: %04X\n", ioctl);
+	printf("length: %d\n", length);
+	printf("dwRecvlen: %d\n", dwRecvlen);
 	printf ("Send buffer: \n");
 	printHex(pin_struct, length);
-
 	rv = SCardControl(hCard, ioctl, pin_struct,
 			length, pucRecvbuf, dwRecvlen, pdwRecvlen);
 
@@ -266,9 +269,73 @@ DLL_LOCAL DWORD sendControl(SCARDHANDLE hCard, int ioctl,
 
 	}
 	else
-	      printf("\tppgemplus-plugin =>  %04x\n", 
+	      printf("\tppgemplus-plugin =>  %08x\n", 
 				(unsigned int)rv);
 	return rv;
+}
+
+DLL_LOCAL DWORD loadStrings(SCARDHANDLE hCard, unsigned char ucPinType)
+{
+
+	/*The Following Blob contains the Portuguese strings to show on the Pinpad Display:
+		PIN Autent.?   
+		Novo PIN?    
+		Conf. novo PIN
+		OK  
+		PIN falhou      
+		Tempo expirou   
+		* tentiv. restam
+		Introduza cartao
+		Erro cartao 
+		PIN bloqueado   
+	*/
+	char stringTable[] =
+		"\xB2\xA0\x00\x4D\x4C\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x4E\x6F\x76"
+		"\x6F\x20\x50\x49\x4E\x3F\x20\x20\x20\x20\x20\x20\x20\x43\x6F\x6E\x66\x2E\x20\x6E\x6F\x76\x6F"
+		"\x20\x50\x49\x4E\x20\x20\x50\x49\x4E\x20\x4F\x4B\x2E\x20\x20\x20\x20\x20\x20\x20\x20\x20\x50"
+		"\x49\x4E\x20\x66\x61\x6C\x68\x6F\x75\x20\x20\x20\x20\x20\x20\x54\x65\x6D\x70\x6F\x20\x65\x78"
+		"\x70\x69\x72\x6F\x75\x20\x20\x20\x2A\x20\x74\x65\x6E\x74\x69\x76\x2E\x20\x72\x65\x73\x74\x61\x6D"
+		"\x49\x6E\x74\x72\x6F\x64\x75\x7A\x61\x20\x63\x61\x72\x74\x61\x6F\x45\x72\x72\x6F\x20\x63\x61\x72"
+		"\x74\x61\x6F\x20\x20\x20\x20\x20\x50\x49\x4E\x20\x62\x6C\x6F\x71\x75\x65\x61\x64\x6F\x20\x20\x20";
+
+	DWORD ioctl = 0x00312000;
+	char recvBuf[200];
+	DWORD rv = 0;
+	unsigned int STRING_LEN = 16;
+	DWORD recvlen = sizeof(recvBuf);
+
+	switch (ucPinType)
+	{
+		case EIDMW_PP_TYPE_AUTH:
+			memcpy(&stringTable[5], "PIN Autent.?    ", STRING_LEN); 
+			break;
+		case EIDMW_PP_TYPE_SIGN:
+			memcpy(&stringTable[5], "PIN Assinatura? ", STRING_LEN); 
+			break;
+
+		case EIDMW_PP_TYPE_ADDR:
+			memcpy(&stringTable[5], "PIN Morada?     ", STRING_LEN); 
+			break;
+	}
+	
+	printHex(stringTable, sizeof(stringTable)-1);
+
+	printf("Size of string table: %d\n", sizeof(stringTable)-1);
+	
+	rv = SCardControl(hCard, ioctl, stringTable, sizeof(stringTable)-1,
+		recvBuf, recvlen, &recvlen);
+
+	
+	if ( rv == SCARD_S_SUCCESS )
+	{
+		printf("Strings Loaded successfully\n");
+	}
+	else
+		printf("Error in LoadStrings: SCardControl() returned: %08x\n", 
+				(unsigned int)rv);
+	
+	return rv;
+
 }
 
 /*
@@ -293,6 +360,8 @@ EIDMW_PP_API long EIDMW_PP2_Command(
 	
 	dwAtrLen = sizeof(pbAtr);
 
+	printf("EIDMW_PP2_Command called with ioctl= %08x\n", ioctl);
+
 	if (ioctl == CM_IOCTL_GET_FEATURE_REQUEST)
 	{
 		printf("ppgemplus-plugin ==> Get Feature Request\n");
@@ -312,10 +381,13 @@ EIDMW_PP_API long EIDMW_PP2_Command(
 			printf (" %02X", pbAtr[i]);
 		printf("\n");
 
+		rv = loadStrings(hCard, ucPintype);
+
 		//For IAS cards we need to VerifyPIN before Modify
 		if (ioctl == CM_IOCTL_VERIFY_PIN || !IsGemsafe(pbAtr, dwAtrLen))
 		{
 			printf("ppgemplus-plugin ==> Verify PIN\n");
+
 			fillVerifyControlStruct(&pin_verify);
 			pin_struct = &pin_verify;
 
@@ -332,6 +404,7 @@ EIDMW_PP_API long EIDMW_PP2_Command(
 				length = fillStructGemsafe(pin_verify.abData, ucPintype, 0);
 
 			}
+
 			rv = sendControl(hCard, ioctl2, pin_struct,
 				length, pucRecvbuf, dwRrecvlen, pdwRrecvlen);
 
