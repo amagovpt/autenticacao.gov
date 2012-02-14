040a5dcc797c751003afe7ea39b75f81dc29ac15
Vasco Silva <vasco.silva@caixamagica.pt>
2012-02-14 10:23:08 +0000

    Remove dont ask for pin on sign patch

--- pteid-mw-pt/_src/eidmw/mdw_online_patches/apply_patches.sh
+++ pteid-mw-pt/_src/eidmw/mdw_online_patches/apply_patches.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #patch -p0 -i build_on_glibc_2.14.patch
-patch -p0 -i dont_ask_for_pin_on_sign.patch
+#patch -p0 -i dont_ask_for_pin_on_sign.patch
 patch -p0 -i use_rpath_origin.patch
