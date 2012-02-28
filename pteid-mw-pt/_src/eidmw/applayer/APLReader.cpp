4c4117bdb0ab0190b94e0a47bf1c1b4133971a69
lmcm <lmcm@caixamagica.pt>
2012-02-28 11:24:59 +0000

    Remove CRL references.

--- pteid-mw-pt/_src/eidmw/applayer/APLReader.cpp
+++ pteid-mw-pt/_src/eidmw/applayer/APLReader.cpp
@@ -38,7 +38,6 @@
 #include "APLCardPteid.h"
 #include "eidErrors.h"
 #include "MWException.h"
-#include "CRLService.h"
 #include "dialogs.h"
 #include "cryptoFwkPteid.h"
 #include "CertStatusCache.h"
@@ -594,7 +593,6 @@ CAppLayer::CAppLayer()
 
 	m_Cal=NULL;
 	m_cryptoFwk=NULL;
-	m_crlDownloadCache=NULL;
 	m_certStatusCache=NULL;
 
 	m_askfortestcard=false;
@@ -681,30 +679,12 @@ void CAppLayer::startAllServices()
 	//Then start the caches (Certificates and CRL)
 	if(!m_certStatusCache)
 		m_certStatusCache = new APL_CertStatusCache(m_cryptoFwk);
-
-	//At least, start the CrlDownloadCache, which will Run CRL service and DownloadControl
-	if(!m_crlDownloadCache)
-		m_crlDownloadCache = new APL_CrlDownloadingCache(m_cryptoFwk);
 }
 
 void CAppLayer::stopAllServices() 
 {
 	//stopping is made in the opposite order then starting
 	MWLOG(LEV_INFO, MOD_APL, L"Stop all applayer services");
-
-	/*if(m_crlDownloadCache)
-	{
-		m_crlDownloadCache->stopAllThreads();
-
-		delete m_crlDownloadCache;
-		m_crlDownloadCache=NULL;
-	}
-
-	if(m_certStatusCache)
-	{
-		delete m_certStatusCache;
-		m_certStatusCache=NULL;
-	}*/
 
 	if(m_cryptoFwk)
 	{
@@ -797,22 +777,6 @@ void CAppLayer::updateVersion()
 		conf_BuildNbr.ChangeLookupBehaviour(APL_Config::USER_ONLY);
 		long build = conf_BuildNbr.getLong();
 		
-		if(build<4876)
-		{
-			APL_Config conf_ValidCrl(CConfig::EIDMW_CONFIG_PARAM_CERTVALID_CRL);     
-			conf_ValidCrl.setLong(0);
-
-			APL_Config conf_ValidOcsp(CConfig::EIDMW_CONFIG_PARAM_CERTVALID_OCSP);     
-			conf_ValidOcsp.setLong(0);
-		}
-
-		/*
-		if(build<...)
-		{
-			...
-		}
-		*/
-
 		conf_BuildNbr.setLong(SVN_REVISION);
 	}
 	catch(...) //If the update failed, we will try next time
