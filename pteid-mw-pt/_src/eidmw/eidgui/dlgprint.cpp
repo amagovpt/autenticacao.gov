b956f1bb8a18501cc8690558ace2a2330da6cf22
lmcm <lmcm@caixamagica.pt>
2012-03-13 18:30:44 +0000

    Fix dialog size on OSX.

--- pteid-mw-pt/_src/eidmw/eidgui/dlgprint.cpp
+++ pteid-mw-pt/_src/eidmw/eidgui/dlgprint.cpp
@@ -43,7 +43,7 @@ dlgPrint::dlgPrint( QWidget* parent, CardInformation& CI_Data, GenPur::UI_LANGUA
     if (CI_Data.isDataLoaded())
     {
 		ui.setupUi(this);
-		setFixedSize(378, 233);
+		setFixedSize(385, 245);
 		const QIcon Ico = QIcon( ":/images/Images/Icons/Print.png" );
 		this->setWindowIcon( Ico );
 
