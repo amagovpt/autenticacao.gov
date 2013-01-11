9909542ac19c95d62badc543a046abc8a5baf969
André Guerreiro <aguerreiro1985@gmail.com>
2013-01-11 12:16:35 +0000

    Bump version in PTEID_PRODUCT_VERSION

--- pteid-mw-pt/_src/eidmw/_Builds/pteidversions.h
+++ pteid-mw-pt/_src/eidmw/_Builds/pteidversions.h
@@ -16,11 +16,11 @@
 #define PTEID_PRODUCT_NAME    	"Portugal eID MiddleWare"
 
 
-#define PTEID_PRODUCT_VERSION    "2.1.1"
-#define BASE_VERSION_STRING    "2, 1, 1, "
+#define PTEID_PRODUCT_VERSION    "2.1.2"
+#define BASE_VERSION_STRING    "2, 1, 2, "
 #define BASE_VERSION1          2
 #define BASE_VERSION2          1
-#define BASE_VERSION3          1
+#define BASE_VERSION3          2
 
 
 // Common Lib
