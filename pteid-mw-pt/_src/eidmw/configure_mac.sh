aafc1303d0188a54974b5fdacacec5242c98c553
André Guerreiro <aguerreiro1985@gmail.com>
2013-11-21 12:07:04 +0000

    Fix QT includes and lflags for MacOS

--- pteid-mw-pt/_src/eidmw/configure_mac.sh
+++ pteid-mw-pt/_src/eidmw/configure_mac.sh
@@ -27,14 +27,15 @@ fi
 
 #if [ ! "$QTBASEDIR" ]
 #then
-#        QTBASEDIR=/usr/local/Qt4.5
+        QTBASEDIR=/Library/Frameworks
 #fi
 #echo "[Info ] Checking $QTBASEDIR"
 
 if [[ "$QTBASEDIR" != "" && -d $QTBASEDIR ]]
 then
         echo "[Info ] $QTBASEDIR found"
-	./configure CONFIG=release QMAKE_LFLAGS+=-F$QTBASEDIR/lib --include=/System/Library/Frameworks/PCSC.framework/Versions/A/Headers/ --include+=$QTBASEDIR/include/QtCore/ --include+=$QTBASEDIR/QtCore.framework/Headers/ --include+=$QTBASEDIR/include/QtGui/  --include+=$QTBASEDIR/QtGui.framework/Headers/ --lib=-L/System/Library/Frameworks/PCSC.framework/ --lib+=-L$QTBASEDIR/lib/QtCore.framework  --lib+=-L$QTBASEDIR/QtCore.framework --lib+=-L$QTBASEDIR/lib/QtGui.framework --lib+=-L$QTBASEDIR/QtGui.framework
+	./configure CONFIG=release QMAKE_LFLAGS+=-F$QTBASEDIR/lib --include=/System/Library/Frameworks/PCSC.framework/Versions/A/Headers/ --include+=$QTBASEDIR/include/QtCore/ --include+=$QTBASEDIR/QtCore.framework/Headers/ --include+=$QTBASEDIR/include/QtGui/  --include+=$QTBASEDIR/QtGui.framework/Headers/ --include+=$QTBASEDIR/QtNetwork.framework/Headers/ --lib=-L/System/Library/Frameworks/PCSC.framework/ --lib+=-L$QTBASEDIR/QtCore.framework/Versions/Current/ --lib+=-L$QTBASEDIR/QtGui.framework/Versions/Current/ --lib+=-L$QTBASEDIR/QtNetwork.framework/Versions/Current/
+
 else
         echo "[Warn ] Qt base directory not found: using defaults"
         echo "[Warn ] Or specify the Qt4.5 basepath as follows: QTBASEDIR=/usr/local/Qt4.5 <script>"
