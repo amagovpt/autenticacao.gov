ea1806caf419ed5dacf4e936c6e5282c2b8bf5cb
Vasco Silva <vasco.silva@caixamagica.pt>
2016-09-19 10:19:51 +0000

    try do dynamicaly get javac path

--- pteid-mw-pt/_src/eidmw/configure_debian.sh
+++ pteid-mw-pt/_src/eidmw/configure_debian.sh
@@ -2,6 +2,10 @@ QT4=`readlink -f \`which qmake\``
 QT4PATH=${QT4%/*}
 QT4PATH=${QT4PATH}/..
 #JAVACPATH=`which javac 2> /dev/null`
+
+# Try to dynamic get javac version
+JAVACPATH=readlink -f `which javac` 
+
 JAVACPATH=/usr/lib/jvm/java-6-sun/bin/javac
 JAVAC=`echo $JAVACPATH | wc -l`
 if [[ $JAVAC -eq 1 ]]
