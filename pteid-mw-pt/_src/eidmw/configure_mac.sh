66a4a49fed1ea3c20d6e94976353100939006ce3
André Guerreiro <aguerreiro1985@gmail.com>
2016-12-20 12:53:47 +0000

    Fix configure script for Mac

--- pteid-mw-pt/_src/eidmw/configure_mac.sh
+++ pteid-mw-pt/_src/eidmw/configure_mac.sh
@@ -4,11 +4,11 @@ OSDEF=__NONE__
 
 #if [ ! "$QTBASEDIR" ]
 #then
-        QTBASEDIR=/usr/local/Cellar/qt/4.8.7/lib/
+        QTBASEDIR=/usr/local/Cellar/qt5/5.7.0/lib/
 #fi
 #echo "[Info ] Checking $QTBASEDIR"
 
-if [[ "$QTBASEDIR" != "" && -d $QTBASEDIR ]]
+if [[ "$QTBASEDIR" != "" ]]
 then
         echo "[Info ] $QTBASEDIR found"
 	./configure CONFIG=release QMAKE_LFLAGS+=-F$QTBASEDIR/lib --include=/System/Library/Frameworks/PCSC.framework/Versions/A/Headers/ --include+=$QTBASEDIR/include/QtCore/ --include+=$QTBASEDIR/QtCore.framework/Headers/ --include+=$QTBASEDIR/include/QtGui/  --include+=$QTBASEDIR/QtGui.framework/Headers/ --include+=$QTBASEDIR/QtNetwork.framework/Headers/ --lib=-L/System/Library/Frameworks/PCSC.framework/ --lib+=-L$QTBASEDIR/QtCore.framework/Versions/Current/ --lib+=-L$QTBASEDIR/QtGui.framework/Versions/Current/ --lib+=-L$QTBASEDIR/QtNetwork.framework/Versions/Current/
