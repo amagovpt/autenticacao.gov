3272433595e0841066e32fb67013af0cdfe9d6c0
André Guerreiro <aguerreiro1985@gmail.com>
2016-12-23 14:51:29 +0000

    SCAP: Load translation file from the application directory not relying on the CWD being set to it

--- pteid-mw-pt/_src/eidmw/scap-client-v2/main.cpp
+++ pteid-mw-pt/_src/eidmw/scap-client-v2/main.cpp
@@ -24,9 +24,10 @@ int main(int argc, char *argv[])
     QString lang = scapSettings.getPTEIDLanguage();
 
     QTranslator translator;
+
     if(lang.compare("nl") == 0) {
         // Loads Portuguese versions
-        bool ret = translator.load("scapsignature_pt");
+        bool ret = translator.load(QCoreApplication::applicationDirPath() + "/" + "scapsignature_pt");
         std::cout << "Loaded Portuguese version sucessfully: " << ret << std::endl;
     }
     a.installTranslator(&translator);
