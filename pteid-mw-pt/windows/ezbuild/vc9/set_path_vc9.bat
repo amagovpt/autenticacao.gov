d5510437484ae2db8458d2ae90532ec33a6ea5a8
André Guerreiro <aguerreiro1985@gmail.com>
2017-05-24 10:22:43 +0000

    Update Windows dependency script

--- pteid-mw-pt/windows/ezbuild/vc9/set_path_vc9.bat
+++ pteid-mw-pt/windows/ezbuild/vc9/set_path_vc9.bat
@@ -1,50 +1,30 @@
 ::@set PTEID_TARGET_RUNTIME=VCR9
-@set PTEID_LIBS_PATH=%~dp0..\..\..\_src\ThirdParty
+
+@set PTEID_POPPLER_QT5=C:\Users\agrr\Downloads\poppler-buildroot\poppler-0.52.0\qt5\src\
 
 :: Compiled / external libs x86
 ::==============================
-@set PTEID_DIR_QT_4=C:\Qt\4.8.4_win32
-@set PTEID_DIR_OPENSSL_098=C:\OpenSSL-Win32\
-@set PTEID_DIR_XERCES_31=C:\Users\agrr\Downloads\xerces-c-3.1.1-x86-windows-vc-9.0
-@set PTEID_DIR_CAIRO=C:\Users\agrr\Downloads\gtk+-bundle_2.22.1-20101227_win32
-@set PTEID_DIR_CURL=C:\Users\agrr\Downloads\libcurl-vc9-x86-release-dll-ipv6-sspi-spnego-winssl
+@set PTEID_DIR_QT_4=C:\Qt-win32\5.6\msvc2013
+@set PTEID_DIR_OPENSSL_098=C:\openssl-1.0.2k-win32
+@set PTEID_DIR_XERCES_31=C:\Users\agrr\Downloads\xerces-c-3.1.4-win32
+@set PTEID_DIR_CURL=C:\Users\agrr\Downloads\curl-7.51.0-win32
 
 :: Compiled / external libs x64
 ::==============================
-@set PTEID_DIR_QT_4_X64=C:\Qt\4.8.4_x64
-@set PTEID_DIR_OPENSSL_098_X64=C:\OpenSSL-Win64\
-@set PTEID_DIR_XERCES_31_X64=C:\Users\agrr\Downloads\xerces-c-3.1.1-x86_64-windows-vc-9.0
-@set PTEID_DIR_CAIRO_X64=C:\Users\agrr\Downloads\gtk+-bundle_2.22.1-20101229_win64
-@set PTEID_DIR_CURL_X64=C:\Users\agrr\Downloads\libcurl-vc9-x64-release-dll-ipv6-sspi-spnego-winssl
+@set PTEID_DIR_QT_4_X64=C:\Qt\5.6\msvc2013_64
+@set PTEID_DIR_OPENSSL_098_X64=C:\openssl-1.0.2k-win64
+@set PTEID_DIR_XERCES_31_X64=C:\Users\agrr\Downloads\xerces-c-3.1.4-win64
+@set PTEID_DIR_CURL_X64=C:\Users\agrr\Downloads\curl-7.51.0-win64
 
 :: Tools
 ::======
 @set PTEID_DIR_7ZIP=C:\Program Files\7-Zip
 @set PTEID_DIR_PLATFORMSDK=C:\Program Files (x86)\Windows Kits\8.1
-@set PTEID_DIR_VS=C:\Program Files (x86)\Microsoft Visual Studio 9.0
+@set PTEID_DIR_VS=C:\Program Files (x86)\Microsoft Visual Studio 12.0
 @set PTEID_DIR_DOXYGEN=C:\Program Files\doxygen
 @set PTEID_DIR_SWIG=C:\Users\agrr\Downloads\swigwin-2.0.12
-@set PTEID_DIR_JDK=C:\Program Files\Java\jdk1.8.0_05
-@set PTEID_DIR_WIX=%WIX%
+@set PTEID_DIR_JDK=C:\Program Files\Java\jdk1.8.0_111
+@set PTEID_DIR_WIX=C:\Program Files (x86)\WiX Toolset v3.10
 @set PTEID_DIR_MSM=C:\Program Files (x86)\Common Files\Merge Modules
 @set PTEID_DRIVERS_DDK=C:\WinDDK\7600.16385.1
 
-:: COMPAT
-::========
-::@set PTEID_DIR_QT_4_EXE=%PTEID_DIR_QT_4%
-::@set PTEID_DIR_QT_4_INCLUDE=%PTEID_DIR_QT_4%
-::@set PTEID_DIR_QT_4_DYNAMIC=%PTEID_DIR_QT_4%
-
-::@set PTEID_DIR_QT_4_EXE_X64=%PTEID_DIR_QT_4_X64%
-::@set PTEID_DIR_QT_4_INCLUDE_X64=%PTEID_DIR_QT_4_X64%
-::@set PTEID_DIR_QT_4_DYNAMIC_X64=%PTEID_DIR_QT_4_X64%
-
-::@set PTEID_DIR_VS_2005=%PTEID_DIR_VS%
-::@set PTEID_DIR_PLATFORMSDK_2003=%PTEID_DIR_PLATFORMSDK%
-::@set PTEID_DIR_PLATFORMSDK_2008=%PTEID_DIR_PLATFORMSDK%
-::@set PTEID_DIR_QT_450_EXE=%PTEID_DIR_QT_4_EXE%
-::@set PTEID_DIR_QT_450_INCLUDE=%PTEID_DIR_QT_4_INCLUDE%
-::@set PTEID_DIR_QT_450_DYNAMIC=%PTEID_DIR_QT_4_DYNAMIC%
-::@set PTEID_DIR_OPENSSL_098G=%PTEID_DIR_OPENSSL_098%
-::@set PTEID_DIR_XERCES_310=%PTEID_DIR_XERCES_31%
-::@set PTEID_DIR_JDK_142=%PTEID_DIR_JDK%
