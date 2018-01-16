645fbb7c842282fe7dbfcbe97e10f207a013fd8b
André Guerreiro <aguerreiro1985@gmail.com>
2018-01-16 10:55:08 +0000

    Add build solution scripts for V3 solution. Having these scripts the separation between V2 and V3 solutions is complete

---
+++ pteid-mw-pt/windows/ezbuild/vc9/set_path_autenticacao.gov.bat
@@ -0,0 +1,29 @@
+
+@set PTEID_POPPLER_QT5=C:\Users\agrr\Downloads\poppler-buildroot\
+@set PTEID_XMLSECURITY=C:\Users\agrr\Downloads\xml-security-c-1.7.3\
+
+:: Compiled / external libs x86
+::==============================
+@set PTEID_DIR_QT_4=
+@set PTEID_DIR_OPENSSL_098=
+@set PTEID_DIR_XERCES_31=
+@set PTEID_DIR_CURL=
+
+:: Compiled / external libs x64
+::==============================
+@set PTEID_DIR_QT_4_X64=
+@set PTEID_DIR_OPENSSL_098_X64=
+@set PTEID_DIR_XERCES_31_X64=
+@set PTEID_DIR_CURL_X64=
+
+:: Tools
+::======
+@set PTEID_DIR_7ZIP=C:\Program Files\7-Zip
+@set PTEID_DIR_PLATFORMSDK=C:\Program Files (x86)\Windows Kits\8.1
+@set PTEID_DIR_VS=C:\Program Files (x86)\Microsoft Visual Studio 12.0
+@set PTEID_DIR_SWIG=
+@set PTEID_DIR_JDK=C:\Program Files\Java\jdk1.8.0_111
+@set PTEID_DIR_WIX=C:\Program Files (x86)\WiX Toolset v3.10
+@set PTEID_DIR_MSM=C:\Program Files (x86)\Common Files\Merge Modules
+@set PTEID_DRIVERS_DDK=C:\WinDDK\7600.16385.1
+
