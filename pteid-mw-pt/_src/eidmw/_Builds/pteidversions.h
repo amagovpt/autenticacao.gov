d8f8496686cc33e2991a9386037d13766aff7f0e
Adriano Campos <adrianoribeirocampos@gmail.com>
2018-08-21 15:14:11 +0000

    Release 5.0.14 - 5560
    - Remove load windows root certificates
    - Add new Root CA certs: COMODO and Camerfirm. They will be used by AMA services and PKI.cartaodecidadao server in the future

--- pteid-mw-pt/_src/eidmw/_Builds/pteidversions.h
+++ pteid-mw-pt/_src/eidmw/_Builds/pteidversions.h
@@ -16,11 +16,11 @@
 #define PTEID_PRODUCT_NAME    	"Portugal eID MiddleWare"
 
 
-#define PTEID_PRODUCT_VERSION    "3.0.13"
-#define BASE_VERSION_STRING    "3, 0, 13, "
+#define PTEID_PRODUCT_VERSION    "3.0.14"
+#define BASE_VERSION_STRING    "3, 0, 14, "
 #define BASE_VERSION1          3
 #define BASE_VERSION2          0
-#define BASE_VERSION3          13
+#define BASE_VERSION3          14
 
 
 // Common Lib
