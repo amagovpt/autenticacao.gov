370ee3c4858c7e043dd09fc5bff935613acdf4c3
Adriano Campos <adrianoribeirocampos@gmail.com>
2018-12-18 16:10:06 +0000

    Clean up set_path scripts

--- pteid-mw-pt/windows/set_path_vc9.bat
+++ pteid-mw-pt/windows/set_path_vc9.bat
@@ -1,27 +1,29 @@
 ::@set PTEID_TARGET_RUNTIME=VCR9
 
-@set PTEID_POPPLER_QT5=C:\Users\agrr\Downloads\poppler
-@set PTEID_XMLSECURITY=C:\Users\agrr\Downloads\xml-security-c-1.7.3\
+@set PTEID_POPPLER_QT5=
+@set PTEID_XMLSECURITY=
 
 :: Compiled / external libs x86
 ::==============================
-@set PTEID_DIR_QT_4=C:\Qt-5.9-win32
-::@set PTEID_DIR_QT_4=C:\Qt-win32\5.6\msvc2013
-@set PTEID_DIR_OPENSSL_098=C:\openssl-1.0.2k-win32
-@set PTEID_DIR_XERCES_31=C:\Users\agrr\Downloads\xerces-c-3.1.4-win32
-@set PTEID_DIR_CURL=C:\Users\agrr\Downloads\curl-7.51.0-win32
+@set PTEID_DIR_QT_4=
+::@set PTEID_DIR_QT_4=
+@set PTEID_DIR_OPENSSL_098=
+@set PTEID_DIR_XERCES_31=
+@set PTEID_DIR_CURL=
 @set PTEID_DIR_ZLIB=
+@set PTEID_DIR_LIBZIP=
 @set PTEID_DIR_LIBPNG=
 @set PTEID_DIR_OPENJPEG=
 
 :: Compiled / external libs x64
 ::==============================
-@set PTEID_DIR_QT_4_X64=C:\Qt-5.9\5.9.1\msvc2013_64
-::@set PTEID_DIR_QT_4_X64=C:\Qt\5.6\msvc2013_64
-@set PTEID_DIR_OPENSSL_098_X64=C:\openssl-1.0.2k-win64
-@set PTEID_DIR_XERCES_31_X64=C:\Users\agrr\Downloads\xerces-c-3.1.4-win64
-@set PTEID_DIR_CURL_X64=C:\Users\agrr\Downloads\curl-7.51.0-win64
+@set PTEID_DIR_QT_4_X64=
+::@set PTEID_DIR_QT_4_X64=
+@set PTEID_DIR_OPENSSL_098_X64=
+@set PTEID_DIR_XERCES_31_X64=
+@set PTEID_DIR_CURL_X64=
 @set PTEID_DIR_ZLIB_X64=
+@set PTEID_DIR_LIBZIP_X64=
 @set PTEID_DIR_LIBPNG_X64=
 @set PTEID_DIR_OPENJPEG_X64=
 
