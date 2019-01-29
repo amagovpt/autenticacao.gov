5b75c8d79116a0fbcd5d55a5218632ca48196f49
Adriano Campos <adrianoribeirocampos@gmail.com>
2019-01-29 10:36:11 +0000

    Release v3.0.16 5819

--- pteid-mw-pt/_src/eidmw/svn_revision.h
+++ pteid-mw-pt/_src/eidmw/svn_revision.h
@@ -20,7 +20,7 @@
 #ifndef __SVN_REVISION_H__
 #define __SVN_REVISION_H__
 
-#define SVN_REVISION 5771
-#define SVN_REVISION_STR "5771"
+#define SVN_REVISION 5819
+#define SVN_REVISION_STR "5819"
 
 #endif //__SVN_REVISION_H__
