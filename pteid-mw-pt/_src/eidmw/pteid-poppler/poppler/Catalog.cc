amagovpt/autenticacao.gov

commit 150e168be8ec056cbbc475e9e3f4d4cb2a1489da

Only add signature appearance dict for "visible" signatures
This way we skip adding a lot of objects to invisible signatures and
resulting signed files are smaller

pteid-mw-pt/_src/eidmw/pteid-poppler/poppler/Catalog.cc
@@ -340,7 +340,7 @@ void Catalog::prepareSignature(PDFRectangle *rect, SignatureSignerInfo *signer_i
 	obj4.initArray (xref);
 	
 	//Visible Signature location 
-	if (rect)
+	if (rect->isValid())
 	{
 		//Check if signature height (y2 - y1) is less than 90 considering rounding errors
 		small_signature_format = (rect->y2 - rect->y1) - 90.0 < -0.00001;
@@ -364,14 +364,17 @@ void Catalog::prepareSignature(PDFRectangle *rect, SignatureSignerInfo *signer_i
 
 	int rotate_signature = page_obj->getRotate();
 
-    if (signer_info->attribute_provider == NULL) {
-			addSignatureAppearance(&signature_field, signer_info, date_with_timezone->getCString(),
-                     location, reason, r2-r0 - 1, r3-r1 -1, img_data, img_length, rotate_signature, isPTLanguage);
-    }
-	else {
-		addSignatureAppearanceSCAP(&signature_field, signer_info, date_with_timezone->getCString(),
-                     location, reason, r2-r0 - 1, r3-r1 -1, img_data, img_length, rotate_signature, isPTLanguage);
-    }
+	//Only add signature appearance for "visible" signatures
+	if (rect->isValid()) {
+		if (signer_info->attribute_provider == NULL) {
+			addSignatureAppearance(&signature_field, signer_info, date_outstr,
+				location, reason, r2-r0 - 1, r3-r1 -1, img_data, img_length, rotate_signature, isPTLanguage);
+		}
+		else {
+			addSignatureAppearanceSCAP(&signature_field, signer_info, date_outstr,
+				location, reason, r2-r0 - 1, r3-r1 -1, img_data, img_length, rotate_signature, isPTLanguage);
+		}
+	}
 
 	//memset(date_outstr, 0, sizeof(date_outstr));
 

commit 5a3768ce458d983cd2f3d204c6b2d8718c4e9a88

Fix regression introduced in 150e168b - wrong visible date format

pteid-mw-pt/_src/eidmw/pteid-poppler/poppler/Catalog.cc
@@ -367,11 +367,11 @@ void Catalog::prepareSignature(PDFRectangle *rect, SignatureSignerInfo *signer_i
 	//Only add signature appearance for "visible" signatures
 	if (rect->isValid()) {
 		if (signer_info->attribute_provider == NULL) {
-			addSignatureAppearance(&signature_field, signer_info, date_outstr,
+			addSignatureAppearance(&signature_field, signer_info, date_with_timezone->getCString(),
 				location, reason, r2-r0 - 1, r3-r1 -1, img_data, img_length, rotate_signature, isPTLanguage);
 		}
 		else {
-			addSignatureAppearanceSCAP(&signature_field, signer_info, date_outstr,
+			addSignatureAppearanceSCAP(&signature_field, signer_info, date_with_timezone->getCString(),
 				location, reason, r2-r0 - 1, r3-r1 -1, img_data, img_length, rotate_signature, isPTLanguage);
 		}
 	}