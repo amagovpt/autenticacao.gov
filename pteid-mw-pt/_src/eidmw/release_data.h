32d609d9f0087c754ab3dcf970ed56296e7a1b4b
Adriano Campos <adrianoribeirocampos@gmail.com>
2019-11-22 14:20:37 +0000

    Update release_data file with v3.0.21 release data

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
 #define PTEID_COPYRIGHT          "Copyright (C) 2010-2019" 
-#define PTEID_PRODUCT_VERSION    "3.0.20" 
-#define BASE_VERSION_STRING      "3, 0, 20, " 
+#define PTEID_PRODUCT_VERSION    "3.0.21" 
+#define BASE_VERSION_STRING      "3, 0, 21, " 
 #define BASE_VERSION1            3 
 #define BASE_VERSION2            0 
-#define BASE_VERSION3            20 
-#define REVISION_NUM             3528 
-#define REVISION_NUM_STRING      "3528" 
-#define REVISION_HASH_STRING     "85419207" 
+#define BASE_VERSION3            21 
+#define REVISION_NUM             7001 
+#define REVISION_NUM_STRING      "7001" 
+#define REVISION_HASH_STRING     "000d99b8" 
 #endif //__RELEASE_DATA_H__ 
