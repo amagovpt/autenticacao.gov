77639a47ed0da2b0e9b799442525c5ddfb6d5a74
José Pinto <jose.pinto@caixamagica.pt>
2020-02-27 14:34:13 +0000

    Allow building with swig versions other than 4 on linux

--- pteid-mw-pt/_src/eidmw/eidlibJava_Wrapper/create_java_files.sh
+++ pteid-mw-pt/_src/eidmw/eidlibJava_Wrapper/create_java_files.sh
@@ -1,1 +1,11 @@
-swig -c++ -java -package pt.gov.cartaodecidadao -doxygen -o ./GeneratedFiles/eidlibJava_Wrapper.cpp -outdir ./GeneratedFiles ../eidlib/eidlib.i
+#!/bin/bash
+swig_version_major="$(swig -version | awk -F " " '/Version/{print $3}' | cut -d"." -f1)"
+cmd="swig -c++ -java -package pt.gov.cartaodecidadao -doxygen -o ./GeneratedFiles/eidlibJava_Wrapper.cpp -outdir ./GeneratedFiles ../eidlib/eidlib.i"
+
+if [ $swig_version_major -lt 4 ];
+then
+    echo "SWIG version < 4. Removing doxygen flag from swig command."
+    cmd="$(echo $cmd | sed "s/ -doxygen//g")"
+fi
+
+$($cmd)
