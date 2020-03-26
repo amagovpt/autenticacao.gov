a1b3dc86037c69fcdf0a5f8fe8ed4362e4a4e1c8
Adriano Campos <adrianoribeirocampos@gmail.com>
2020-03-26 13:08:11 +0000

    Release 3.1.0 - 3830 - 46a1659f

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
-#define PTEID_COPYRIGHT          "Copyright (C) 2010-2019" 
-#define PTEID_PRODUCT_VERSION    "3.0.21" 
-#define BASE_VERSION_STRING      "3, 0, 21, " 
+#define PTEID_COPYRIGHT          "Copyright (C) 2010-2020" 
+#define PTEID_PRODUCT_VERSION    "3.1.0" 
+#define BASE_VERSION_STRING      "3, 1, 0, " 
 #define BASE_VERSION1            3 
-#define BASE_VERSION2            0 
-#define BASE_VERSION3            21 
-#define REVISION_NUM             7001 
-#define REVISION_NUM_STRING      "7001" 
-#define REVISION_HASH_STRING     "000d99b8" 
+#define BASE_VERSION2            1 
+#define BASE_VERSION3            0 
+#define REVISION_NUM             3830 
+#define REVISION_NUM_STRING      "3830" 
+#define REVISION_HASH_STRING     "46a1659f" 
 #endif //__RELEASE_DATA_H__ 
