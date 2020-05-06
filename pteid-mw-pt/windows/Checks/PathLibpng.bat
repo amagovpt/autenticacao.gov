7c9fecb8d434e25c584f22e844f1af46b99e8043
Cristiano Lopes <e.mail@cristianolopes.eu>
2020-05-06 17:44:00 +0100

    Remove check for .bsc files as they are no longer required on more recent Visual Studio versions
    They are nicer to the cpu but they complicate the build unecessarly
    https://stackoverflow.com/a/565578/1185339

--- pteid-mw-pt/windows/Checks/PathLibpng.bat
+++ pteid-mw-pt/windows/Checks/PathLibpng.bat
@@ -1,5 +1,5 @@
 @echo [INFO] Input PTEID_DIR_LIBPNG=%PTEID_DIR_LIBPNG%
-@set FILE_TO_FIND="bin\libpng16.dll" "lib\libpng16.lib" "lib\libpng16.exp" "lib\libpng16.bsc"
+@set FILE_TO_FIND="bin\libpng16.dll" "lib\libpng16.lib" "lib\libpng16.exp"
 @echo [INFO] Looking for files: %FILE_TO_FIND%
 
 @set FILE_NOT_FOUND=
