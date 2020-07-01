d1744ca70fd1b3a39b7474db89f8050bf9e0db7e
Andre Guerreiro <aguerreiro1985@gmail.com>
2020-07-01 14:51:54 +0100

    Release 3.2.0 - 3925

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
 #define PTEID_COPYRIGHT          "Copyright (C) 2010-2020" 
-#define PTEID_PRODUCT_VERSION    "3.1.2" 
-#define BASE_VERSION_STRING      "3, 1, 2, " 
+#define PTEID_PRODUCT_VERSION    "3.2.0" 
+#define BASE_VERSION_STRING      "3, 2, 0, " 
 #define BASE_VERSION1            3 
-#define BASE_VERSION2            1 
-#define BASE_VERSION3            2 
-#define REVISION_NUM             3831 
-#define REVISION_NUM_STRING      "3831" 
-#define REVISION_HASH_STRING     "04873c4c" 
+#define BASE_VERSION2            2 
+#define BASE_VERSION3            0 
+#define REVISION_NUM             3925
+#define REVISION_NUM_STRING      "3925" 
+#define REVISION_HASH_STRING     "4c09ce1a" 
 #endif //__RELEASE_DATA_H__ 
