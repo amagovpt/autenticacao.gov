dc7a1400ec5556597a88379b9af471ae8e42f278
Adriano Campos <adriano.campos@caixamagica.pt>
2020-10-07 17:11:31 +0100

    Release 3.3.0 - 3963 - 29303367

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
 #define PTEID_COPYRIGHT          "Copyright (C) 2010-2020" 
-#define PTEID_PRODUCT_VERSION    "3.2.0" 
-#define BASE_VERSION_STRING      "3, 2, 0, " 
+#define PTEID_PRODUCT_VERSION    "3.3.0" 
+#define BASE_VERSION_STRING      "3, 3, 0, " 
 #define BASE_VERSION1            3 
-#define BASE_VERSION2            2 
+#define BASE_VERSION2            3 
 #define BASE_VERSION3            0 
-#define REVISION_NUM             3925
-#define REVISION_NUM_STRING      "3925" 
-#define REVISION_HASH_STRING     "4c09ce1a" 
+#define REVISION_NUM             3963 
+#define REVISION_NUM_STRING      "3963" 
+#define REVISION_HASH_STRING     "29303367" 
 #endif //__RELEASE_DATA_H__ 
