483b2866e291829d0dc4c03cfca020e9ee2647c5
Adriano Campos <adrianoribeirocampos@gmail.com>
2020-11-04 10:19:41 +0000

    Release 3.3.1 - 4000 - 3095b81

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
 #define PTEID_COPYRIGHT          "Copyright (C) 2010-2020" 
-#define PTEID_PRODUCT_VERSION    "3.3.0" 
-#define BASE_VERSION_STRING      "3, 3, 0, " 
+#define PTEID_PRODUCT_VERSION    "3.3.1" 
+#define BASE_VERSION_STRING      "3, 3, 1, " 
 #define BASE_VERSION1            3 
 #define BASE_VERSION2            3 
-#define BASE_VERSION3            0 
-#define REVISION_NUM             3963 
-#define REVISION_NUM_STRING      "3963" 
-#define REVISION_HASH_STRING     "29303367" 
+#define BASE_VERSION3            1 
+#define REVISION_NUM             4000 
+#define REVISION_NUM_STRING      "4000" 
+#define REVISION_HASH_STRING     "3095b81e" 
 #endif //__RELEASE_DATA_H__ 
