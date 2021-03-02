amagovpt/autenticacao.gov

commit eedaea42f8f57234dbb3d06755bb8b3c373d188d

Fix HTTP response reading in read_from_stream()
Also added logging of received HTTP status for SCAP postSoapRequest().
This fixes infinite blocking on SCAP requests with expired certificates
which return small http responses which are fetched in a single
SSL_read() call.

pteid-mw-pt/_src/eidmw/applayer/SSLConnection.cpp
@@ -1318,6 +1318,7 @@ unsigned int SSLConnection::read_from_stream(SSL* ssl, NetworkBuffer *net_buffer
 	int r = -1;
 	unsigned int bytes_read = 0, header_len = 0, content_length = 0;
 	unsigned int current_buf_length = net_buffer->buf_size;
+	char end_of_headers_token[] = {0x0d, 0x0a, 0x0d, 0x0a};
 
 	do
 	{
@@ -1326,10 +1327,17 @@ unsigned int SSLConnection::read_from_stream(SSL* ssl, NetworkBuffer *net_buffer
 		if (r > 0)
 		{
 			if (bytes_read == 0) {
-				header_len = r;
+
 				char * buffer_tmp = (char*)calloc(strlen(net_buffer->buf) + 1, 1);
 				strcpy(buffer_tmp, net_buffer->buf);
 				content_length = parseContentLength(buffer_tmp);
+				if (content_length > 0 && header_len == 0) {
+					char *end_of_headers = strstr(net_buffer->buf, end_of_headers_token);
+					if (end_of_headers) {
+						header_len = (end_of_headers-net_buffer->buf) + sizeof(end_of_headers_token);
+					}
+				}
+
 				free(buffer_tmp);
 			}
 

pteid-mw-pt/_src/eidmw/applayer/ScapSSLConnection.cpp
@@ -36,6 +36,24 @@ namespace eIDMW
             
 	}
 
+	char * get_status_line(char * full_http_response) {
+		char * ptr = full_http_response;
+		size_t first_line_len = 0;
+		while(*ptr && *ptr != '\r' && *ptr != '\n') {
+			first_line_len++;
+			ptr++;
+		}
+
+		if (first_line_len == 0 || strlen(full_http_response)== first_line_len) {
+			return full_http_response;
+		}
+
+		char * status_line = (char *) calloc(first_line_len+1, sizeof(char));
+		memcpy(status_line, full_http_response, first_line_len);
+
+		return status_line;
+	}
+
 	#define HEADERS_BUFSIZE 1000
 
 	char * ScapSSLConnection::postSoapRequest(char *endpoint, char *soapAction, char *soapBody)
@@ -77,7 +95,13 @@ namespace eIDMW
 		}
 
 		MWLOG(LEV_DEBUG, MOD_APL, "Server reply (size=%d): \n", bytes_read);
-		/*MWLOG(LEV_DEBUG, MOD_APL, "Server reply (size=%d): \n%s\n", bytes_read, buffer.buf);*/
+		if (bytes_read > 0) {
+			char * http_status = get_status_line(buffer.buf);
+			MWLOG(LEV_DEBUG, MOD_APL, "Server returned HTTP status: %s", http_status);
+			if (http_status != buffer.buf) {
+				free(http_status);
+			}
+		}
 
 		return buffer.buf;
 	}

commit 643cde934e16b54dc4b4024a277f43d792948d33

Fix regression introduced in eedaea42 for HTTP replies with Content-Length and small log improvements
End of headers location was failing because the second parameter passed to strstr() was not a null-terminated string

pteid-mw-pt/_src/eidmw/applayer/SSLConnection.cpp
@@ -721,7 +721,7 @@ DHParamsResponse *SSLConnection::do_SAM_1stpost(DHParams *p, char *secretCode, c
 	
 	if (ret > 0)
 	{
-		MWLOG(LEV_DEBUG, MOD_APL, "do_SAM_1stpost: Server reply: %s", buffer.buf);
+		MWLOG(LEV_DEBUG, MOD_APL, "do_SAM_1stpost: Server reply (%u bytes): %s", ret, buffer.buf);
 		m_session_cookie = parseCookie(buffer.buf);
 		if (m_session_cookie == NULL)
 		{
@@ -1308,8 +1308,6 @@ void SSLConnection::read_chunked_reply(SSL *ssl, NetworkBuffer *net_buffer, bool
     }
     while(bytes_read == 0 || !final_chunk_read);
 
-	MWLOG(LEV_DEBUG, MOD_APL, "Server chunked reply (size=%d): ", bytes_read);
-
     //Extract the HTTP body of the chunked message
     parse_http_chunked_body(net_buffer);
 
@@ -1324,7 +1322,8 @@ unsigned int SSLConnection::read_from_stream(SSL* ssl, NetworkBuffer *net_buffer
 	int r = -1;
 	unsigned int bytes_read = 0, header_len = 0, content_length = 0;
 	unsigned int current_buf_length = net_buffer->buf_size;
-	char end_of_headers_token[] = {0x0d, 0x0a, 0x0d, 0x0a};
+	const char * eoh_token     = "\r\n\r\n";
+	const size_t eoh_token_len = 4;
 
 	do
 	{
@@ -1338,9 +1337,9 @@ unsigned int SSLConnection::read_from_stream(SSL* ssl, NetworkBuffer *net_buffer
 				strcpy(buffer_tmp, net_buffer->buf);
 				content_length = parseContentLength(buffer_tmp);
 				if (content_length > 0 && header_len == 0) {
-					char *end_of_headers = strstr(net_buffer->buf, end_of_headers_token);
+					char *end_of_headers = strstr(net_buffer->buf, eoh_token);
 					if (end_of_headers) {
-						header_len = (end_of_headers-net_buffer->buf) + sizeof(end_of_headers_token);
+						header_len = (end_of_headers-net_buffer->buf) + eoh_token_len;
 					}
 				}
 

pteid-mw-pt/_src/eidmw/applayer/ScapSSLConnection.cpp
@@ -91,9 +91,10 @@ namespace eIDMW
 		{
 			MWLOG(LEV_DEBUG, MOD_APL, "ScapSSLConnection: server response is chunked, trying read_chunked_reply()");
 			read_chunked_reply(m_ssl_connection, &buffer, true);
+			MWLOG(LEV_DEBUG, MOD_APL, "Server chunked reply (size=%d)", strlen(buffer.buf));
 
 		} else {
-			MWLOG(LEV_DEBUG, MOD_APL, "Server reply (size=%d): ", bytes_read);
+			MWLOG(LEV_DEBUG, MOD_APL, "Server reply (size=%d)", bytes_read);
 		}
 
 		if (bytes_read > 0) {