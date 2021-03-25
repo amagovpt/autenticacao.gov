89b36629190e48a9b88944b281e8cc01f4648a3c
Adriano Campos <adrianoribeirocampos@gmail.com>
2021-03-25 13:05:22 +0000

    Release 3.5.0 - 4271

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
-#define PTEID_COPYRIGHT          "Copyright (C) 2010-2020" 
-#define PTEID_PRODUCT_VERSION    "3.4.0" 
-#define BASE_VERSION_STRING      "3, 4, 0, " 
+#define PTEID_COPYRIGHT          "Copyright (C) 2010-2021" 
+#define PTEID_PRODUCT_VERSION    "3.5.0" 
+#define BASE_VERSION_STRING      "3, 5, 0, " 
 #define BASE_VERSION1            3 
-#define BASE_VERSION2            4 
+#define BASE_VERSION2            5 
 #define BASE_VERSION3            0 
-#define REVISION_NUM             4151 
-#define REVISION_NUM_STRING      "4151" 
-#define REVISION_HASH_STRING     "bae92373" 
+#define REVISION_NUM             4271 
+#define REVISION_NUM_STRING      "4271" 
+#define REVISION_HASH_STRING     "3b9a9429" 
 #endif //__RELEASE_DATA_H__ 
