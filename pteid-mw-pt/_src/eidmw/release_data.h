7dfaa332a3d6a4a6020efad9fedfd4de7358242f
José Pinto <jose.pinto@caixamagica.pt>
2021-04-30 17:24:25 +0100

    Release 3.6.0 - 4315

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
 #define PTEID_COPYRIGHT          "Copyright (C) 2010-2021" 
-#define PTEID_PRODUCT_VERSION    "3.5.0" 
-#define BASE_VERSION_STRING      "3, 5, 0, " 
+#define PTEID_PRODUCT_VERSION    "3.6.0" 
+#define BASE_VERSION_STRING      "3, 6, 0, " 
 #define BASE_VERSION1            3 
-#define BASE_VERSION2            5 
+#define BASE_VERSION2            6 
 #define BASE_VERSION3            0 
-#define REVISION_NUM             4271 
-#define REVISION_NUM_STRING      "4271" 
-#define REVISION_HASH_STRING     "3b9a9429" 
+#define REVISION_NUM             4315 
+#define REVISION_NUM_STRING      "4315" 
+#define REVISION_HASH_STRING     "f1a0e309" 
 #endif //__RELEASE_DATA_H__ 
