90ae56753262eb35830410d2146da9a9150ada3d
José Pinto <jose.pinto@caixamagica.pt>
2021-05-26 16:49:24 +0100

    Fix log message for when writing zip with log files fails

--- pteid-mw-pt/_src/eidmw/eidguiV2/appcontroller.cpp
+++ pteid-mw-pt/_src/eidmw/eidguiV2/appcontroller.cpp
@@ -910,7 +910,8 @@ void AppController::zipLogs() {
     if (zip_close(pZip) < 0)
     {
         free(pZip);
-        PTEID_LOG(PTEID_LOG_LEVEL_ERROR, "eidgui", "AppController::zipLog: zip_close failed with error %d", zip_error_strerror(zip_get_error(pZip)));
+        PTEID_LOG(PTEID_LOG_LEVEL_ERROR, "eidgui", "AppController::zipLog: zip_close failed with error %s",
+            zip_error_strerror(zip_get_error(pZip)));
         emit signalZipLogsFail();
         return;
     }
