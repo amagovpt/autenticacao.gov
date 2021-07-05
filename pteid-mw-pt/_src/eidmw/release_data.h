65afe9f2580f50006a62c81411afd7d159745e6c
André Guerreiro <aguerreiro1985@gmail.com>
2021-07-05 11:32:20 +0100

    Release 3.6.1 - 4338
    Windows-only bugfix release

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
 #define PTEID_COPYRIGHT          "Copyright (C) 2010-2021" 
-#define PTEID_PRODUCT_VERSION    "3.6.0" 
-#define BASE_VERSION_STRING      "3, 6, 0, " 
+#define PTEID_PRODUCT_VERSION    "3.6.1" 
+#define BASE_VERSION_STRING      "3, 6, 1, " 
 #define BASE_VERSION1            3 
 #define BASE_VERSION2            6 
-#define BASE_VERSION3            0 
-#define REVISION_NUM             4315 
-#define REVISION_NUM_STRING      "4315" 
-#define REVISION_HASH_STRING     "f1a0e309" 
+#define BASE_VERSION3            1 
+#define REVISION_NUM             4338 
+#define REVISION_NUM_STRING      "4338" 
+#define REVISION_HASH_STRING     "bf204135" 
 #endif //__RELEASE_DATA_H__ 
