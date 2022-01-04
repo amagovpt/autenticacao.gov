dedd98e96bbaa57806e48943416499846534ffc9
André Guerreiro <aguerreiro1985@gmail.com>
2022-01-04 16:00:29 +0000

    Update doc and build script on the NodeJS module and remove old build artifacts

--- pteid-mw-pt/_src/eidmw/eidlibNodeJS_Wrapper/generate_swig_wrapper.sh
+++ pteid-mw-pt/_src/eidmw/eidlibNodeJS_Wrapper/generate_swig_wrapper.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+if ! test -d GeneratedFiles
+then
+    mkdir GeneratedFiles
+fi
 
-# This swig3 binary was compiled from the repo https://github.com/oliver----/swig-v8 
-# The current stable version can't yet generated javascrip / nodejs wrappers.
-
-swig3 -v -c++ -javascript -node -o ./GeneratedFiles/eidlibNodeJS_Wrapper.cpp ../eidlib/eidlib.i
+swig -v -c++ -javascript -node -o ./GeneratedFiles/eidlibNodeJS_Wrapper.cpp ../eidlib/eidlib.i
+node-gyp configure
+node-gyp build
