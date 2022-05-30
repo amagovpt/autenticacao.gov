6effeef19c3fba234e67e350c9c6147812666ac2
Adriano Campos <adrianoribeirocampos@gmail.com>
2022-05-30 09:30:10 +0100

    Prepare release 3.8.0 - 4656

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
-#define PTEID_COPYRIGHT          "Copyright (C) 2010-2021" 
-#define PTEID_PRODUCT_VERSION    "3.7.0" 
-#define BASE_VERSION_STRING      "3, 7, 0, " 
+#define PTEID_COPYRIGHT          "Copyright (C) 2010-2022" 
+#define PTEID_PRODUCT_VERSION    "3.8.0" 
+#define BASE_VERSION_STRING      "3, 8, 0, " 
 #define BASE_VERSION1            3 
-#define BASE_VERSION2            7 
+#define BASE_VERSION2            8 
 #define BASE_VERSION3            0 
-#define REVISION_NUM             4491 
-#define REVISION_NUM_STRING      "4491" 
-#define REVISION_HASH_STRING     "2096f19ed" 
+#define REVISION_NUM             4656 
+#define REVISION_NUM_STRING      "4656" 
+#define REVISION_HASH_STRING     "d253af5a1" 
 #endif //__RELEASE_DATA_H__ 
