4d79b6581b6e6773675742218a4771b8c5dd1934
Daniel Dron <daniel.dron@caixamagica.pt>
2022-06-23 08:48:50 +0100

    Fix scanDir implementation inconsistence

--- pteid-mw-pt/_src/eidmw/common/Util.cpp
+++ pteid-mw-pt/_src/eidmw/common/Util.cpp
@@ -518,7 +518,8 @@ void scanDir(const char *Dir, const char *SubDir, const char *Ext, bool &bStopRe
 					{
 						// this is a regular file
 						std::string file = pFile->d_name;
-						std::string ext = Ext;
+						std::string ext = ".";
+						ext += Ext;
 						// check if the file has the requested extension
 						if (strlen(Ext) == 0 || (file.size() > ext.size() && file.compare(file.size() - ext.size(), ext.size(), ext) == 0))
 						{
