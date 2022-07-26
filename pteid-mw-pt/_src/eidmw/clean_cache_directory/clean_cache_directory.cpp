34fa560585328a3ef97701da8ba508208dd8bb82
Daniel Dron <daniel.dron@caixamagica.pt>
2022-07-26 09:53:59 +0100

    Improve windows cache cleanup on uninstall

--- pteid-mw-pt/_src/eidmw/clean_cache_directory/clean_cache_directory.cpp
+++ pteid-mw-pt/_src/eidmw/clean_cache_directory/clean_cache_directory.cpp
@@ -1,10 +1,46 @@
-#include <iostream>
-#include <direct.h>
-#include <assert.h>
 #include <windows.h> 
-#include <lm.h>
+#include <Shlwapi.h>
+#include <vector>
+#include <algorithm>
+#include <functional>
 
 #define MAX_KEY_LENGTH 255
+
+void loopFiles(const std::wstring &directoryPath, std::wstring mask, std::function<bool(const std::wstring &fullFilePath, const std::wstring &fileName)> callback)
+{
+	WIN32_FIND_DATA ffd;
+	HANDLE hFind;
+	DWORD a = 0;
+
+	std::wstring path = directoryPath;
+	path += L"\\" + mask;
+
+	hFind = FindFirstFile(path.c_str(), &ffd);
+	if (hFind == INVALID_HANDLE_VALUE)
+		return;
+
+	do
+	{
+		if (wcscmp(ffd.cFileName, L".") != 0 || wcscmp(ffd.cFileName, L"..") != 0)
+		{
+			std::wstring fullFilePath = directoryPath;
+			fullFilePath += L"\\";
+			fullFilePath += ffd.cFileName;
+
+			if (ffd.dwFileAttributes != FILE_ATTRIBUTE_DIRECTORY)
+				if (callback(fullFilePath, ffd.cFileName))
+					break;
+		}
+	} while (FindNextFile(hFind, &ffd));
+
+	FindClose(hFind);
+}
+
+std::wstring getFileExtension(const std::wstring &fileName)
+{
+	auto i = fileName.find(L".");
+	return fileName.substr(i);
+}
 
 bool getAppDataDirectoryForUser(TCHAR *userSID, _Out_ std::wstring &value)
 {
@@ -77,20 +113,43 @@ int main()
 			bool foundAppData = getAppDataDirectoryForUser(achKey, appDataDirectory);
 
 			std::wstring cacheFilesDirectoryPath = appDataDirectory + L"\\.pteid-ng";
+			std::wstring eidmwCacheDirectoryPath = appDataDirectory + L"\\.eidmwcache";
+			std::wstring scapAttributesDirectoryPath = eidmwCacheDirectoryPath + L"\\scap_attributes";
+
 			if (foundAppData)
 			{
-				//Delete folder and all its contents
-				SHFILEOPSTRUCT file_op = {
-					NULL,
-					FO_DELETE,
-					cacheFilesDirectoryPath.c_str(),
-					L"",
-					FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_SILENT,
-					false,
-					0,
-					L""
-				};
-				SHFileOperation(&file_op);
+				if (PathFileExists(cacheFilesDirectoryPath.c_str()))
+				{
+					//Delete all files with these extensions
+					std::vector<std::wstring> extensions = { L".ebin", L".bin" };
+					//Delete all files with these names
+					std::vector<std::wstring> files = { L"updateCertsLog.txt", L"updateNewsLog.txt" };
+					loopFiles(cacheFilesDirectoryPath, L"*.*", [&](const std::wstring &fullFilePath, const std::wstring &fileName) {
+						auto fileExt = getFileExtension(fileName);
+						if (std::find(extensions.begin(), extensions.end(), fileExt) != extensions.end())
+							DeleteFile(fullFilePath.c_str());
+
+						if (std::find(files.begin(), files.end(), fileName) != files.end())
+							DeleteFile(fullFilePath.c_str());
+
+						return false;
+					});
+				}
+
+				if (PathFileExists(eidmwCacheDirectoryPath.c_str()) && PathFileExists(scapAttributesDirectoryPath.c_str()))
+				{
+					loopFiles(scapAttributesDirectoryPath, L"*.xml", [&](const std::wstring &fullFilePath, const std::wstring &fileName) {
+						DeleteFile(fullFilePath.c_str());
+						return false;
+					});
+				}
+
+				if (PathIsDirectoryEmpty(cacheFilesDirectoryPath.c_str()))
+					RemoveDirectory(cacheFilesDirectoryPath.c_str());
+				if (PathIsDirectoryEmpty(eidmwCacheDirectoryPath.c_str()))
+					RemoveDirectory(eidmwCacheDirectoryPath.c_str());
+				if (PathIsDirectoryEmpty(scapAttributesDirectoryPath.c_str()))
+					RemoveDirectory(scapAttributesDirectoryPath.c_str());
 			}
 		}
 	}
