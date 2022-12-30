8fdf1b1efa3c7f96e4070502419251ea616085e4
André Guerreiro <aguerreiro1985@gmail.com>
2022-12-30 16:32:46 +0000

    Release 3.9.1 - 4852

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
 #define PTEID_COPYRIGHT          "Copyright (C) 2010-2022" 
-#define PTEID_PRODUCT_VERSION    "3.9.0" 
-#define BASE_VERSION_STRING      "3, 9, 0, " 
+#define PTEID_PRODUCT_VERSION    "3.9.1" 
+#define BASE_VERSION_STRING      "3, 9, 1, " 
 #define BASE_VERSION1            3 
 #define BASE_VERSION2            9 
-#define BASE_VERSION3            0 
-#define REVISION_NUM             4837 
-#define REVISION_NUM_STRING      "4837" 
-#define REVISION_HASH_STRING     "d1a702220" 
+#define BASE_VERSION3            1 
+#define REVISION_NUM             4852 
+#define REVISION_NUM_STRING      "4852" 
+#define REVISION_HASH_STRING     "aceedb5e1" 
 #endif //__RELEASE_DATA_H__ 
