amagovpt/autenticacao.gov

commit 23cd0a77e89f49396378372320357b57c74533b3

Load the lato font from a file distributed with the app on Linux
The flatpak package should be as self-contained as possible so we can't depend on fonts being available in the container host system

pteid-mw-pt/_src/eidmw/eidguiV2/main.qml
@@ -36,25 +36,12 @@ Window {
 
     title: "Autenticação.Gov"
 
-    FontLoader { 
+    FontLoader {
         id: lato;
         name: "Lato" // This avoids printing error on application start
 
-        source: {
-            // on linux, load installed font by name
-            // on windows and macOS use source property to point to distributed font file
-            if (Qt.platform.os === "windows" || Qt.platform.os === "osx"){
-                controler.getFontFile("lato")
-            }
-            else ""
-        }
+        source: ""
     }
-
-
-
-
-
-
 
     onWidthChanged: {
         //console.log("Resizing app width: " + width + "height" + height)

pteid-mw-pt/_src/eidmw/dialogs/dialogsQTsrv/main.cpp
@@ -164,13 +164,10 @@ QString getPinName( DlgPinUsage usage, wchar_t *inPinName ) {
 
 QFont getLatoFont(){
 	QFont font;
-#ifdef __APPLE__
 	QString fontFile = ":/fonts/lato/Lato-Regular.ttf";
 	int id = QFontDatabase::addApplicationFont(fontFile);
 	font = QFontDatabase::applicationFontFamilies(id).at(0);
-#else
-	font = QFont("lato");
-#endif
+
 	return font;
 }
 

pteid-mw-pt/_src/eidmw/eidguiV2/eidguiV2.pro
@@ -116,6 +116,7 @@ translations.files += eidmw_en.qm \
 fonts.path = $${INSTALL_DIR_BIN}/../share/pteid-mw/fonts
 fonts.files += fonts/myriad/MyriadPro-Regular.otf
 fonts.files += fonts/myriad/MyriadPro-Bold.otf
+fonts.files += fonts/lato/Lato-Regular.ttf
 
 INSTALLS += target translations fonts
 

pteid-mw-pt/_src/eidmw/eidguiV2/main.cpp
@@ -15,6 +15,7 @@
 #include <QQuickStyle>
 #include <QIcon>
 #include <QProcess>
+#include <QFontDatabase>
 #include "appcontroller.h"
 #include "gapi.h"
 #include "eidlib.h"
@@ -271,6 +272,10 @@ int main(int argc, char *argv[])
         // Load translation files
         gapi.initTranslation();
         controller.initTranslation();
+#ifdef __linux__
+        int font_id = QFontDatabase::addApplicationFont(QCoreApplication::applicationDirPath()+"/../share/pteid-mw/fonts/Lato-Regular.ttf");
+        qDebug() << "Font id: " << font_id;
+#endif
 
         // Load main QML file
         engine->load(QUrl(MAIN_QML_PATH));

commit aceedb5e189ef337354c1467452333f17f4c1476

Fix font loading regression introduced in 23cd0a77e89f49396378372320357b57c74533b3
That change should only affect font loading on Linux

pteid-mw-pt/_src/eidmw/eidguiV2/main.qml
@@ -40,7 +40,14 @@ Window {
         id: lato;
         name: "Lato" // This avoids printing error on application start
 
-        source: ""
+        source: {
+            // on linux, load installed font by name
+            // on windows and macOS use source property to point to distributed font file
+            if (Qt.platform.os === "windows" || Qt.platform.os === "osx") {
+                controler.getFontFile("lato")
+            }
+            else ""
+        }
     }
 
     onWidthChanged: {