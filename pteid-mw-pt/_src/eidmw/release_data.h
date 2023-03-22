9e1873bfc154ec41ac77f0a365f04dcba7cc2845
André Guerreiro <aguerreiro1985@gmail.com>
2023-03-22 14:28:41 +0000

    Update 3.10.0 release date
    An urgent fix was needed so we delayed the release

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -4,9 +4,9 @@
 #define PTEID_PRODUCT_VERSION    "3.10.0" 
 #define BASE_VERSION_STRING      "3, 10, 0, " 
 #define BASE_VERSION1            3 
-#define BASE_VERSION2            10
-#define BASE_VERSION3            0
-#define REVISION_NUM             4920
-#define REVISION_NUM_STRING      "4920" 
-#define REVISION_HASH_STRING     "49d988032" 
+#define BASE_VERSION2            10 
+#define BASE_VERSION3            0 
+#define REVISION_NUM             4941 
+#define REVISION_NUM_STRING      "4941" 
+#define REVISION_HASH_STRING     "fb1cbf637" 
 #endif //__RELEASE_DATA_H__ 
