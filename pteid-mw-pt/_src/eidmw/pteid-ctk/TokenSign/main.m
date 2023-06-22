6ef1b951855226f33c7b42656b4c7bcbb9c71994
André Guerreiro <aguerreiro1985@gmail.com>
2023-06-22 10:52:08 +0100

    Remove TokenSign empty program
    Example code for PteidToken is available in docs.autenticacao.gov repository

--- pteid-mw-pt/_src/eidmw/pteid-ctk/TokenSign/main.m
+++
@@ -1,9 +0,0 @@
-//
-//  main.m
-//  TokenSign - signature commandline utility to test PteidToken module
-//
-
-#import <Foundation/Foundation.h>
-
-//TODO: search for the KeyChain items with SecItemCopyMatching
-//TODO: sign with SecKeyCreateSignature
