08fad3532c90b4977e05e97f8586be8391c17f92
Miguel Mota <42717736+UnlimitedPugWorks@users.noreply.github.com>
2024-03-01 09:08:37 +0000

    Update documentation generation script for Java SDK

--- pteid-mw-pt/_src/eidmw/eidlibJava_Wrapper/gen_docs.sh
+++ pteid-mw-pt/_src/eidmw/eidlibJava_Wrapper/gen_docs.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 # generate javadocs and zip it
-javadoc -d ./javadocs/ -sourcepath ./GeneratedFiles/Callback.java ./GeneratedFiles/*.java ./GeneratedFiles/pteidlibJava_*.java ./src/*.java -quiet -overview index.html --allow-script-in-comments && \ 
-zip -r9 javadocs.zip ./javadocs/
+rm -r ./javadocs/
+# These 3 following files are deleted to exclude them from appearing in the documentation
+rm -r ./GeneratedFiles/pteidlibJava_Wrapper.java
+rm -r ./GeneratedFiles/pteidlibJava_WrapperJNI.java
+rm -r ./GeneratedFiles/SWIGTYPE_p_unsigned_long.java
+javadoc -d ./javadocs/ -sourcepath ./GeneratedFiles/Callback.java ./GeneratedFiles/*.java -quiet -public -overview index.html --allow-script-in-comments && zip -r9 javadocs.zip ./javadocs/
+# The deleted files are generated again
+sudo ./create_java_files.sh
