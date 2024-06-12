022f1ac138472080482e5756fd5ac4774d9623e3
Rui Teixeira <rui.teixeira@caixamagica.pt>
2024-06-12 10:23:13 +0100

    Eidlibdotnet: Compile on x86_x64 and arm64

--- pteid-mw-pt/_src/eidmw/eidlibdotnetsdk/generate_cs.sh
+++ pteid-mw-pt/_src/eidmw/eidlibdotnetsdk/generate_cs.sh
@@ -15,6 +15,6 @@ echo "Revision number: $REVISION_NUM"
 
 dotnet build -c Release -r osx-x64
 
-clang++ -shared -std=c++17 -fPIC -I../eidlib -I../CMD/services -I../applayer -I../common -I../cardlayer -I../dialogs -L../lib -I$HOME/mw-thirdparty-libs/openssl-3/include -lpteidcommon -lpteiddialogsQT -lpteidcardlayer -lpteidapplayer -lCMDServices ../eidlib/*.cpp ../eidlibCS_Wrapper/GeneratedFiles/eidlibCS_Wrapper.cpp -o libdotnetsdk_x64.dylib
+clang++ -shared -arch x86_64 -std=c++17 -fPIC -I../eidlib -I../CMD/services -I../applayer -I../common -I../cardlayer -I../dialogs -L../lib -I$HOME/mw-thirdparty-libs/openssl-3/include -lpteidcommon -lpteiddialogsQT -lpteidcardlayer -lpteidapplayer -lCMDServices ../eidlib/*.cpp ../eidlibCS_Wrapper/GeneratedFiles/eidlibCS_Wrapper.cpp -o libdotnetsdk_x64.dylib
 clang++ -shared -arch arm64 -std=c++17 -fPIC -I../eidlib -I../CMD/services -I../applayer -I../common -I../cardlayer -I../dialogs -L../lib -I$HOME/mw-thirdparty-libs/openssl-3/include -lpteidcommon -lpteiddialogsQT -lpteidcardlayer -lpteidapplayer -lCMDServices ../eidlib/*.cpp ../eidlibCS_Wrapper/GeneratedFiles/eidlibCS_Wrapper.cpp -o libdotnetsdk_arm.dylib
 lipo -create libdotnetsdk_x64.dylib libdotnetsdk_arm.dylib -output libdotnetsdk.dylib
