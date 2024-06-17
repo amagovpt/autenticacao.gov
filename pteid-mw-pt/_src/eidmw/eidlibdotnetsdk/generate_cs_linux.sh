161000b3008a72cb49d8c8464c479163cd79ed44
Rui Teixeira <rui.teixeira@caixamagica.pt>
2024-06-17 14:19:13 +0100

    Eidlibdotnet: Add script to generate .so and .dll to dotnetsdk on linux

---
+++ pteid-mw-pt/_src/eidmw/eidlibdotnetsdk/generate_cs_linux.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+set -v
+
+mkdir -p "../eidlibCS_Wrapper/GeneratedFiles"
+
+swig -c++ -csharp -namespace pt.portugal.eid -dllimport libdotnetsdk -o ../eidlibCS_Wrapper/GeneratedFiles/eidlibCS_Wrapper.cpp -DSWIG2_CSHARP -DSWIG_CSHARP_NO_STRING_HELPER -outdir ../eidlibCS_Wrapper/GeneratedFiles "../eidlib/eidlib.i" 
+
+REVISION_NUM=$(git rev-list --count HEAD)
+
+export REVISION_NUM
+
+echo "Revision number: $REVISION_NUM"
+
+dotnet build -c Release
+
+g++ -std=c++17 -Wl,--no-as-needed -shared -fPIC -I../eidlib -I../CMD/services -I../applayer -I../common -I../cardlayer -I../dialogs -I$/usr/include -L../lib -lpteidcommon -lpteiddialogsQT -lpteidcardlayer -lpteidapplayer -lCMDServices ../eidlib/*.cpp ../eidlibCS_Wrapper/GeneratedFiles/eidlibCS_Wrapper.cpp -o libdotnetsdk.so
+
+cp libdotnetsdk.so ../lib
+cp bin/Release/net8.0/libdotnetsdk.dll ../lib
