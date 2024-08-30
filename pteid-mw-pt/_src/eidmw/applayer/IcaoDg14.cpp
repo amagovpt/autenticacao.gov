8bfb099c15ca49d1cd33f571a43741abb21b9358
André Guerreiro <aguerreiro1985@gmail.com>
2024-08-30 17:09:44 +0100

    Fix DG14 parser log message

--- pteid-mw-pt/_src/eidmw/applayer/IcaoDg14.cpp
+++ pteid-mw-pt/_src/eidmw/applayer/IcaoDg14.cpp
@@ -44,7 +44,7 @@ SecurityInfos* decodeDg14Data(const CByteArray& data) {
 		errString = ERR_error_string(errCode, NULL);
 		MWLOG(LEV_ERROR, MOD_APL, "%s: openssl error: %s generated in %s line %d", errString, errFile, errLine);
     } else {
-		MWLOG(LEV_DEBUG, MOD_APL, "%s: The decoder consumed %ld bytes", __FUNCTION__, asn1_data - data.GetBytes());
+		MWLOG(LEV_DEBUG, MOD_APL, "%s: The decoder consumed %ld bytes", __FUNCTION__, asn1_data - data_copy.GetBytes());
     }
 
     return security_infos;
