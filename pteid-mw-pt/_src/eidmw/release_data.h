a01844783e0c74d699fbb5e9143b533d2f0d6fa1
André Guerreiro <aguerreiro1985@gmail.com>
2026-03-23 19:29:13 +0000

    Release 3.15.0

--- pteid-mw-pt/_src/eidmw/release_data.h
+++ pteid-mw-pt/_src/eidmw/release_data.h
@@ -1,12 +1,12 @@
 #ifndef __RELEASE_DATA_H__ 
 #define __RELEASE_DATA_H__ 
-#define PTEID_COPYRIGHT          "Copyright (C) 2010-2025" 
-#define PTEID_PRODUCT_VERSION    "3.14.0" 
-#define BASE_VERSION_STRING      "3, 14, 0, " 
+#define PTEID_COPYRIGHT          "Copyright (C) 2010-2026" 
+#define PTEID_PRODUCT_VERSION    "3.15.0" 
+#define BASE_VERSION_STRING      "3, 15, 0, " 
 #define BASE_VERSION1            3 
-#define BASE_VERSION2            14 
+#define BASE_VERSION2            15 
 #define BASE_VERSION3            0 
-#define REVISION_NUM             5939
-#define REVISION_NUM_STRING      "5939" 
-#define REVISION_HASH_STRING     "f83d3ad26" 
+#define REVISION_NUM             6055 
+#define REVISION_NUM_STRING      "6055" 
+#define REVISION_HASH_STRING     "13b56cef4" 
 #endif //__RELEASE_DATA_H__ 
