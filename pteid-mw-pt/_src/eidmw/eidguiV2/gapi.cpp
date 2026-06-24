70b37838c20168f549b00af06fb3d913d213246a
André Guerreiro <aguerreiro1985@gmail.com>
2026-06-24 16:35:03 +0100

    HTTP 403 is valid server status for telemetry requests

--- pteid-mw-pt/_src/eidmw/eidguiV2/gapi.cpp
+++ pteid-mw-pt/_src/eidmw/eidguiV2/gapi.cpp
@@ -309,7 +309,7 @@ void GAPI::doUpdateTelemetry(TelemetryAction action) {
 		qDebug() << "updateTelemetry HTTP status:" << http_code << "for URL:" << url << "Using proxy?: " << using_proxy;
 		if (action == TelemetryAction::Accepted || action == TelemetryAction::Denied) {
 			setTelemetryStatus(
-				http_code == 200
+				(http_code == 200 || http_code == 403)
 					? (action == TelemetryAction::Accepted ? TelemetryStatus::Enabled : TelemetryStatus::Disabled)
 					: (action == TelemetryAction::Accepted ? TelemetryStatus::RetryEnable
 														   : TelemetryStatus::RetryDisable));
