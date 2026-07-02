84502e80b57da2a6b8c586b414ae789aa315a28f
André Guerreiro <aguerreiro1985@gmail.com>
2026-07-02 16:00:36 +0100

    cmd-KSP: Improve new error message

--- pteid-mw-pt/_src/eidmw/cmd-ksp/Provider/language.h
+++ pteid-mw-pt/_src/eidmw/cmd-ksp/Provider/language.h
@@ -39,8 +39,8 @@ DEFINE_LANG_STR(NoCMDCredentials) = {L"The Citizen Card Middleware installed doe
 									 L"Obtenha um instalador oficial em https://www.autenticacao.gov.pt.",
 									 L"", L""};
 DEFINE_LANG_STR(ValidatingCertificate) = {L"Validating certificate...", L"A validar certificado..."};
-DEFINE_LANG_STR(VerifySignatureFailed) = {L"Failed to create signature, please open the middleware and reinstall CMD signature certificate.", 
-																				L"Falhou na criação da assinatura por favor abra o Middleware e\nreinstale o certificado de assinatura da CMD." };
+DEFINE_LANG_STR(VerifySignatureFailed) = {L"Failed to create signature: please open Autenticação.gov and reinstall CMD signature certificate.", 
+																				L"Erro na criação da assinatura: por favor abra a aplicação Autenticação.gov e\nregiste de novo o certificado de assinatura da CMD." };
 } // namespace eIDMW
 
 #endif // __CMDKSP_LANGUAGE_H__
